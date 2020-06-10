e4a13140feef3473e55c4c557941b364459fff5c
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-06-10 14:49:22 -0400

    commentario

--- Synchronizer.js
+++ Synchronizer.js
@@ -1,45 +1,45 @@
 class Synchronizer {
   constructor(viewerArray) {
-    let syncedViewers = [];
-    let activeViewerId = null;
+    let syncedViewers = []; // array of synchronized viewers
+    let activeViewerId = null; // magic init
 
-    // Loop through array of n-viewers
-    viewerArray.forEach(function (elem) {
-      let currentNv = elem;
-      // let locked = currentNv.getLocked()
-      let currentViewer = currentNv.getViewer();
-      let id = currentViewer.id;
+  // Loop through array of n-viewers
+  viewerArray.forEach(function(elem) {
+      // Created viewer already.
+      let currentViewer = elem.getViewer();
 
+      // Add handlers
       currentViewer.addHandler('pan', handler);
       currentViewer.addHandler('zoom', handler);
 
       function handler() {
-        // My viewer is clicked, I'm the active viewer
-        if (activeViewerId == null) {
-          activeViewerId = id;
-        }
-        if (activeViewerId !== id) {
-          return;
-        }
-
-        // As for everybody else...
-        syncedViewers.forEach(function (item) {
-          let view = item.getViewer()
-          if (view.id === id) {
-            return;
+          // My viewer is clicked, I'm the active viewer
+          // start magic
+          if (activeViewerId == null) {
+              activeViewerId = currentViewer.id;
           }
-
-          // other viewers' coords set to my coordinates
-          // unless they are locked
-          if (!item.getLocked()) {
-            view.viewport.zoomTo(currentViewer.viewport.getZoom());
-            view.viewport.panTo(currentViewer.viewport.getCenter());
+          if (activeViewerId !== currentViewer.id) {
+              return;
           }
+          // end magic
 
-        });
-        activeViewerId = null;
+          // As for everybody else...
+          syncedViewers.forEach(function(item) {
+              let view = item.getViewer()
+              if (view.id === currentViewer.id) {
+                  return;
+              }
+              // other viewers' coords set to my coordinates
+              // unless they are locked
+              if (!item.getLocked()) {
+                  view.viewport.zoomTo(currentViewer.viewport.getZoom());
+                  view.viewport.panTo(currentViewer.viewport.getCenter());
+              }
+          });
+          // magic support
+          activeViewerId = null;
       }
-      syncedViewers.push(currentNv);
+      syncedViewers.push(elem);  // add our [viewer] to our list
     });
 
     this.setLocked = function (idx, bool) {
