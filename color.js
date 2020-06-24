799060f74150f1b4923a50dccd2de95827563a93
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-06-24 11:53:59 -0400

    wip

--- color.js
+++ color.js
@@ -1,40 +1,55 @@
 function Color(elem) {
-    var HEX = CP.HEX; // Old hex color parser
+    color_hex = function () {
+        var HEX = CP.HEX; // Old hex color parser
 
-    CP.HEX = function (x) {
-        x = HEX(x);
-        if ('string' === typeof x) {
-            var count = x.length;
-            if (9 === count && x[1] === x[2] && x[3] === x[4] && x[5] === x[6] && x[7] === x[8]) {
-                // Shorten!
-                return x[0] + x[1] + x[3] + x[5] + x[7];
+        CP.HEX = function (x) {
+            x = HEX(x);
+            if ('string' === typeof x) {
+                var count = x.length;
+                if (9 === count && x[1] === x[2] && x[3] === x[4] && x[5] === x[6] && x[7] === x[8]) {
+                    // Shorten!
+                    return x[0] + x[1] + x[3] + x[5] + x[7];
+                }
+                if (7 === count && x[1] === x[2] && x[3] === x[4] && x[5] === x[6]) {
+                    // Shorten!
+                    return x[0] + x[1] + x[3] + x[5];
+                }
             }
-            if (7 === count && x[1] === x[2] && x[3] === x[4] && x[5] === x[6]) {
-                // Shorten!
-                return x[0] + x[1] + x[3] + x[5];
-            }
-        }
-        return x;
-    };
+            return x;
+        };
 
-    var picker = new CP(elem);
+        var picker = new CP(elem);
+        picker.on('change', function (r, g, b, a) {
+            this.source.value = this.color(r, g, b, a);
+        });
+    }
 
-    // Disable the default blur and focus behavior
-    picker.on('blur', function() {});
-    picker.on('focus', function() {});
+    events = function () {
+        var picker = new CP(elem);
 
-    picker.on('change', function (r, g, b, a) {
-        this.source.value = this.color(r, g, b, a);
-        this.source.style.backgroundColor = this.color(r, g, b, a);
-    });
+        // Disable the default blur and focus behavior
+        picker.on('blur', function () { });
+        picker.on('focus', function () { });
 
-    picker.source.addEventListener('click', function(e) {
-        picker.enter();
-        e.stopPropagation();
-    }, false);
+        // Set color value and style
+        picker.on('change', function (r, g, b, a) {
+            this.source.value = this.color(r, g, b, a);
+            this.source.style.backgroundColor = this.color(r, g, b, a);
+        });
 
-    document.documentElement.addEventListener('click', function() {
-        picker.exit();
-    }, false);
-    
+        // Show and hide color picker panel with a click
+        picker.source.addEventListener('click', function (e) {
+            picker.enter();
+            e.stopPropagation();
+        }, false);
+        document.documentElement.addEventListener('click', function () {
+            picker.exit();
+        }, false);
+    }
+
+    function name(params) {
+        
+    }
+    events();
+
 }
