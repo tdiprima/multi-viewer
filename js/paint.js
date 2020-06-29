95fb6a73b24d328df55d9ca8e23e33f355d0b52c
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-06-29 12:31:27 -0400

    brush size

--- js/paint.js
+++ js/paint.js
@@ -1,25 +1,72 @@
 // paint handler
 function Paint(button, viewer) {
 
-    isRealValue = function (obj) {
-        return obj && obj !== 'null' && obj !== 'undefined';
-    }
-
     const overlay = viewer.fabricjsOverlay({
         scale: 1000
     });
+
     const canvas = overlay.fabricCanvas();
     const paintBrush = overlay.fabricCanvas().freeDrawingBrush;
-
     const idx = button.id.trim(-1).replace("btnDraw", "");
     const mark = document.getElementById('mark' + idx);
     paintBrush.color = mark.innerHTML;
 
-    // button event listener
+    function isRealValue(obj) {
+        return obj && obj !== 'null' && obj !== 'undefined';
+    }
+
+    function between(x, min, max) {
+        return x >= min && x <= max;
+    }
+
+    function customizeControls(lineDrawn) {
+        lineDrawn['hasControls'] = false;
+        lineDrawn['hasBorders'] = false;
+        lineDrawn['evented'] = false;
+        // let arr = canvas.getObjects();
+        // lineDrawn is canvas.item(n)
+    }
+
+    function saveCoordinates(lineDrawn) {
+        // TBA
+        // console.log("PATH:\n" + lineDrawn.path);
+    }
+
+    function setBrushWidth(viewer) {
+        let viewportZoom = viewer.viewport.getZoom();
+        let x = viewer.viewport.viewportToImageZoom(viewportZoom);
+        let b;
+
+        if (between(x, 0.69, 1.1)) {
+            b = 1;
+        }
+        if (between(x, 0, 0.35)) {
+            b = 2;
+        }
+        if (between(x, 0, 0.18)) {
+            b = 3;
+        }
+        if (between(x, 0, 0.086)) {
+            b = 4;
+        }
+        if (between(x, 0, 0.043)) {
+            b = 5;
+        }
+        if (between(x, 0, 0.022)) {
+            b = 6;
+        }
+
+        // console.log('brush', b);
+        paintBrush.width = b;
+
+    }
+
+    // EVENT LISTENERS
+
     // DRAWING START
     button.addEventListener('click', function () {
         paintBrush.color = mark.innerHTML;
-        paintBrush.width = 3;
+        setBrushWidth(viewer)
         viewer.setMouseNavEnabled(false);
         viewer.outerTracker.setTracking(false);
         canvas.isDrawingMode = true;
@@ -43,17 +90,4 @@ function Paint(button, viewer) {
             }
         }
     });
-
-    function customizeControls(lineDrawn) {
-        lineDrawn['hasControls'] = false;
-        lineDrawn['hasBorders'] = false;
-        lineDrawn['evented'] = false;
-        // let arr = canvas.getObjects();
-        // lineDrawn is canvas.item(n)
-    }
-
-    function saveCoordinates(lineDrawn) {
-        // TBA
-        // console.log("PATH:\n" + lineDrawn.path);
-    }
 }
