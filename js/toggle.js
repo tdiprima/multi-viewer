59fcb5263c37b505a4645a6c586e827ce5147a85
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-07-20 15:40:32 -0400

    space

--- js/toggle.js
+++ js/toggle.js
@@ -12,7 +12,7 @@ function Toggle(tagName, id, label) {
 
     this.show = function show() {
         return `<input type="${this.tagName}" id="${this.id}" checked>
-         <label for="${this.id}">${this.label}</label>&nbsp;`;
+         <label for="${this.id}">${this.label}</label>&nbsp;&nbsp;`;
     }
 
 }
