407c843881239464acb160a1836d5707cd3d8be6
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-08-04 16:07:18 -0400

    :arrow_up: Upgraded openseadragon-fabricjs-overlay.js

--- js/vendor/fabric/openseadragon-fabricjs-overlay.js
+++ js/vendor/fabric/openseadragon-fabricjs-overlay.js
@@ -1,5 +1,7 @@
-// OpenSeadragon canvas Overlay plugin 0.0.2 based on svg overlay plugin
-
+/**
+ * OpenSeadragon canvas Overlay plugin based on svg overlay plugin and fabric.js
+ * @version 0.0.2
+ */
 (function () {
 
     if (!window.OpenSeadragon) {
@@ -8,32 +10,60 @@
     }
 
     /**
+     * Adds fabric.js overlay capability to your OpenSeadragon Viewer
+     *
      * @param {Object} options
-     *      Allows configurable properties to be entirely specified by passing
-     *      an options object to the constructor.
+     *     Allows configurable properties to be entirely specified by passing
+     *     an options object to the constructor.
+     *
      * @param {Number} options.scale
-     *      Fabric 'virtual' canvas size, for creating objects
-     **/
+     *     Fabric 'virtual' canvas size, for creating objects
+     *
+     * @returns {Overlay}
+     */
     OpenSeadragon.Viewer.prototype.fabricjsOverlay = function (options) {
-
+        // Default
         this._fabricjsOverlayInfo = new Overlay(this);
-        this._fabricjsOverlayInfo._scale = options.scale;
+        this._fabricjsOverlayInfo._scale = 1000;
+
+        // Set if options
+        if (options) {
+            if (options.static) {
+                this._fabricjsOverlayInfo = new Overlay(this, options.static);
+            } else {
+                this._fabricjsOverlayInfo = new Overlay(this, false);
+            }
+
+            if (options.scale) {
+                this._fabricjsOverlayInfo._scale = options.scale; // arbitrary scale for created fabric canvas
+            }
+            else {
+                this._fabricjsOverlayInfo._scale = 1000;
+            }
+        }
 
         return this._fabricjsOverlayInfo;
     };
 
-    // static counter for multiple overlays differentiation
-    var counter = (function () {
-        var i = 1;
+    /**
+     * Static counter for multiple overlays differentiation
+     * @type {function(): number}
+     */
+    let counter = (function () {
+        let i = 1;
 
         return function () {
             return i++;
         }
     })();
 
-    // ----------
-    var Overlay = function (viewer) {
-        var self = this;
+    /**
+     * Overlay object
+     * @param viewer
+     * @constructor
+     */
+    let Overlay = function (viewer, staticCanvas) {
+        let self = this;
 
         this._viewer = viewer;
 
@@ -42,8 +72,8 @@
 
         this._canvasdiv = document.createElement('div');
         this._canvasdiv.style.position = 'absolute';
-        this._canvasdiv.style.left = 0;
-        this._canvasdiv.style.top = 0;
+        this._canvasdiv.style.left = "0px";
+        this._canvasdiv.style.top = "0px";
         this._canvasdiv.style.width = '100%';
         this._canvasdiv.style.height = '100%';
         this._viewer.canvas.appendChild(this._canvasdiv);
@@ -54,12 +84,21 @@
         this._canvas.setAttribute('id', this._id);
         this._canvasdiv.appendChild(this._canvas);
         this.resize();
-        this._fabricCanvas = new fabric.Canvas(this._canvas);
 
-        // disable fabric selection because default click is tracked by OSD
+        // make the canvas static if specified, ordinary otherwise
+        if (staticCanvas) {
+            this._fabricCanvas = new fabric.StaticCanvas(this._canvas);
+        }
+        else {
+            this._fabricCanvas = new fabric.Canvas(this._canvas);
+        }
+
+        // Disable fabric selection because default click is tracked by OSD
         this._fabricCanvas.selection = false;
-        
-        // prevent OSD click elements on fabric objects
+
+        /**
+         * Prevent OSD mousedown on fabric objects
+         */
         this._fabricCanvas.on('mouse:down', function (options) {
             if (options.target) {
                 options.e.preventDefaultAction = true;
@@ -68,6 +107,9 @@
             }
         });
 
+        /**
+         * Prevent OSD mouseup on fabric objects
+         */
         this._fabricCanvas.on('mouse:up', function (options) {
             if (options.target) {
                 options.e.preventDefaultAction = true;
@@ -76,32 +118,52 @@
             }
         });
 
+        /**
+         * Update viewport
+         */
         this._viewer.addHandler('update-viewport', function () {
             self.resize();
-            self.resizecanvas();
+            self.resizeCanvas();
             self.render();
 
         });
 
+        /**
+         * Resize the fabric.js overlay when the viewer or window changes size
+         */
         this._viewer.addHandler('open', function () {
             self.resize();
-            self.resizecanvas();
+            self.resizeCanvas();
+        });
+        window.addEventListener('resize', function () {
+            self.resize();
+            self.resizeCanvas();
         });
 
     };
 
-    // ----------
+
+    /**
+     * Overlay prototype
+     * {{canvas: (function(): HTMLCanvasElement),
+     * fabricCanvas: (function(): *),
+     * clear: Overlay.clear,
+     * resizeCanvas: Overlay.resizeCanvas,
+     * resize: Overlay.resize,
+     * render: Overlay.render}}
+     */
     Overlay.prototype = {
         // ----------
         canvas: function () {
             return this._canvas;
         },
         fabricCanvas: function () {
+            // Returns fabric.js canvas that you can add elements to
             return this._fabricCanvas;
         },
         // ----------
         clear: function () {
-            this._fabricCanvas.clearAll();
+            this._fabricCanvas.clear();
         },
         render: function () {
             this._fabricCanvas.renderAll();
@@ -113,7 +175,6 @@
                 this._canvasdiv.setAttribute('width', this._containerWidth);
                 this._canvas.setAttribute('width', this._containerWidth);
             }
-
             if (this._containerHeight !== this._viewer.container.clientHeight) {
                 this._containerHeight = this._viewer.container.clientHeight;
                 this._canvasdiv.setAttribute('height', this._containerHeight);
@@ -121,25 +182,22 @@
             }
 
         },
-        resizecanvas: function () {
-
-            var origin = new OpenSeadragon.Point(0, 0);
-            var viewportZoom = this._viewer.viewport.getZoom(true);
+        resizeCanvas: function () {
+            let origin = new OpenSeadragon.Point(0, 0);
+            let viewportZoom = this._viewer.viewport.getZoom(true);
             this._fabricCanvas.setWidth(this._containerWidth);
             this._fabricCanvas.setHeight(this._containerHeight);
-            var zoom = this._viewer.viewport._containerInnerSize.x * viewportZoom / this._scale;
-            this._fabricCanvas.setZoom(zoom);
-            var viewportWindowPoint = this._viewer.viewport.viewportToWindowCoordinates(origin);
-            var x = Math.round(viewportWindowPoint.x);
-            var y = Math.round(viewportWindowPoint.y);
-            var canvasOffset = this._canvasdiv.getBoundingClientRect();
-
-            var pageScroll = OpenSeadragon.getPageScroll();
-
+            // let zoom = this._viewer.viewport._containerInnerSize.x * viewportZoom / this._scale;
+            // this._fabricCanvas.setZoom(zoom);
+            this._fabricCanvas.setZoom(viewportZoom);
+            let viewportWindowPoint = this._viewer.viewport.viewportToWindowCoordinates(origin);
+            let x = Math.round(viewportWindowPoint.x);
+            let y = Math.round(viewportWindowPoint.y);
+            let canvasOffset = this._canvasdiv.getBoundingClientRect();
+            let pageScroll = OpenSeadragon.getPageScroll();
             this._fabricCanvas.absolutePan(new fabric.Point(canvasOffset.left - x + pageScroll.x, canvasOffset.top - y + pageScroll.y));
 
         }
-
     };
 
 })();
