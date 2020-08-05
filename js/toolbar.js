4f4a1abcbb3665a0768a46c0ce1fbc2ac27cbb6c
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-08-05 17:31:50 -0400

    Comments

--- js/toolbar.js
+++ js/toolbar.js
@@ -7,26 +7,33 @@
  * @constructor
  */
 function Toolbar(div, idx, sliders, options) {
-    // Create.
+    // Pan and Zoom checkboxes
     let chkPan1 = `<input type="checkbox" id="chkPan${idx}" checked><label for="chkPan${idx}">Match Pan</label>&nbsp;&nbsp;`
     let chkZoom1 = `<input type="checkbox" id="chkZoom${idx}" checked><label for="chkZoom${idx}">Match Zoom</label>&nbsp;&nbsp;`
+
+    // Slider button
     let btnSlide = `<button class="btn" id="btnSlide${idx}"><i class="fa fa-sliders"></i> Show sliders</button>&nbsp;&nbsp;`;
 
     let color = "#00f";
     if (options.paintbrushColor) { color = options.paintbrushColor; }
 
+    // Paint/Draw buttons handled by paint.js
     let wPaint = `<mark id='mark${idx}'>${options.paintbrushColor}</mark>&nbsp;&nbsp;`;
     let btnDraw = `<button class="btn" id="btnDraw${idx}"><i class="fa fa-pencil"></i> Draw</button>&nbsp;&nbsp;`;
+
+    // Map markers handled by map-marker.js
     let btnMapMarker = `<button class="btn" id="toggle-overlay" style="display: none"><i class="fa fa-map-marker"></i> Hide markers</button>&nbsp;&nbsp;`;
+
+    // Polygon - NEW!
     let btnPolygon = `<button class="btn" id="btnPolygon${idx}"><i class='fas fa-draw-polygon'></i> Draw Polygon</button>&nbsp;&nbsp;`;
     // let btnPolygon = `<button class="btn" id="btnPolygon${idx}"><i style='font-size:24px' class='fas'>&#xf5ee;</i> Draw Polygon</button>&nbsp;&nbsp;`;
 
-    // Draw.
+    // Attach toolbar to viewer
     let div1 = document.createElement('div');
     div1.innerHTML = (options.multipleOn ? chkPan1 + chkZoom1 : "") + wPaint + btnDraw + (options.slidersOn ? btnSlide : "") + btnPolygon + btnMapMarker;
     div.appendChild(div1);
 
-    // Event listeners
+    // Slider button event handler
     if (options.slidersOn) {
         let sld = document.getElementById("btnSlide" + idx);
         if (sld !== null) {
@@ -46,6 +53,7 @@ function Toolbar(div, idx, sliders, options) {
         }
     }
 
+    // TODO: Toggle markers on and off.
     function setMarkerStyle(b) {
         let x = document.getElementsByClassName("fa fa-map-marker");
         let s, h;
@@ -68,5 +76,6 @@ function Toolbar(div, idx, sliders, options) {
 
     }
 
-    let jsc = new Color(document.getElementById('mark' + idx));
+    // Color picker event handler
+    new Color(document.getElementById('mark' + idx));
 }
