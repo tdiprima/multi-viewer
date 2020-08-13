106f927f03f0ea3e2743bbc83849d6ae5ca66767
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-08-13 15:10:58 -0400

    :package:  improved

--- js/packager.sh
+++ js/packager.sh
@@ -2,5 +2,8 @@
 
 rm myPackage.js ultraviewer.min.js
 cat color.js demo.js dropdown.js locationPin.js markupTools.js nViewer.js synchronizer.js toolbar.js >myPackage.js
-$HOME/Documents/workspace/minify/bin/minifyjs myPackage.js >ultraviewer.min.js
-subl ultraviewer.author.js ultraviewer.min.js
+$HOME/Documents/workspace/minify/bin/minifyjs myPackage.js >temp.js
+
+cat ultraviewer.author.js temp.js >ultraviewer.min.js
+rm temp.js myPackage.js
+subl ultraviewer.min.js
