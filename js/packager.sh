a3dcf36095751968efe30171df2b0ddf0351a8f2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-08-20 11:00:06 -0400

    Update packager.sh

--- js/packager.sh
+++ js/packager.sh
@@ -2,6 +2,7 @@
 #$HOME/Documents/workspace/minify/bin/minifyjs myPackage.js >temp.js
 
 rm ultraviewer.min.js
-find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; > ultraviewer.min.js
+find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; > output
+mv output ultraviewer.min.js
 subl ultraviewer.min.js lib/ultraviewer.author.js
 open https://javascript-minifier.com/
