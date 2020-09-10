989ba8bf262ac7889556c4017e9d12c865159224
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-09-10 11:29:22 -0400

    wip

--- js/packager.sh
+++ js/packager.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
 rm ultraviewer.min.js
-#find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; > output
-
-cat color.js dropdown.js pageSetup.js edit.js locationPin.js markupTools.js nViewer.js synchronizer.js > output
+find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; > output
+# cat color.js dropdown.js pageSetup.js edit.js locationPin.js markupTools.js nViewer.js synchronizer.js > output
 mv output ultraviewer.min.js
 
 # $HOME/Documents/workspace/minify/bin/minifyjs ultraviewer.min.js >temp.js
 # rm ultraviewer.min.js; mv temp.js ultraviewer.min.js
 
-#subl ultraviewer.min.js lib/ultraviewer.author.js
-#open https://javascript-minifier.com/
+subl ultraviewer.min.js lib/ultraviewer.author.js
+open https://javascript-minifier.com/
