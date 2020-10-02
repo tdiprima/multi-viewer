36cfadce74d4787c1d1c10bc24914728808cbc74
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-02 11:08:33 -0400

    Contradicted my no-comment rule...
    Comments need to be here in this case, for understandability.
    There's a lot going on, and some fabric quirks that if you're not
    familiar with them, may cause some trouble.

--- js/editPolygon.js
+++ js/editPolygon.js
@@ -1,8 +1,9 @@
-// Locate the controls.
+// Custom position handling
+// Code borrowed from: http://fabricjs.com/custom-controls-polygon
 function polygonPositionHandler (dim, finalMatrix, fabricObject) {
-  // Do for all points
-  const x = fabricObject.points[this.pointIndex].x - fabricObject.pathOffset.x
-  const y = fabricObject.points[this.pointIndex].y - fabricObject.pathOffset.y
+  // This function looks at the pointIndex of the control and returns the current canvas position for that particular point.
+  const x = (fabricObject.points[this.pointIndex].x - fabricObject.pathOffset.x)
+  const y = (fabricObject.points[this.pointIndex].y - fabricObject.pathOffset.y)
   return fabric.util.transformPoint(
     { x: x, y: y },
     fabric.util.multiplyTransformMatrices(
@@ -12,54 +13,36 @@ function polygonPositionHandler (dim, finalMatrix, fabricObject) {
   )
 }
 
-// This function will be called on every mouse move after a control has been
-// clicked and is being dragged.
+// Custom action handler makes the control change the current point.
 function actionHandler (eventData, transform, x, y) {
-  // polygon.__corner is the handle that you dragged.
   const polygon = transform.target
   const currentControl = polygon.controls[polygon.__corner]
-  const mouseLocalPosition = polygon.toLocalPoint(
-    new fabric.Point(x, y),
-    'center',
-    'center'
-  )
+  const mouseLocalPosition = polygon.toLocalPoint(new fabric.Point(x, y), 'center', 'center')
   const polygonBaseSize = polygon._getNonTransformedDimensions()
   const size = polygon._getTransformedDimensions(0, 0)
   const finalPointPosition = {
-    x:
-      (mouseLocalPosition.x * polygonBaseSize.x) / size.x +
-      polygon.pathOffset.x,
-    y:
-      (mouseLocalPosition.y * polygonBaseSize.y) / size.y +
-      polygon.pathOffset.y
+    x: mouseLocalPosition.x * polygonBaseSize.x / size.x + polygon.pathOffset.x,
+    y: mouseLocalPosition.y * polygonBaseSize.y / size.y + polygon.pathOffset.y
   }
   polygon.points[currentControl.pointIndex] = finalPointPosition
   return true
 }
 
-// Keep the polygon in the same position when we change its
-// width/height/top/left.
+// Handles the object that changes dimensions, while maintaining the correct position.
 function anchorWrapper (anchorIndex, fn) {
-  // Once per button click when edit = yes.
   return function (eventData, transform, x, y) {
     const fabricObject = transform.target
-    const absolutePoint = fabric.util.transformPoint(
-      {
-        x: fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x,
-        y: fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y
-      },
-      fabricObject.calcTransformMatrix()
-    )
+    const absolutePoint = fabric.util.transformPoint({
+      x: (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x),
+      y: (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y)
+    }, fabricObject.calcTransformMatrix())
     const actionPerformed = fn(eventData, transform, x, y)
-    // IMPORTANT! Fabric is using newDim!! Do not touch!!
+    // IMPORTANT!  VARIABLE 'newDim' NEEDS TO EXIST.
+    // (Otherwise, the bounding box gets borked):
     const newDim = fabricObject._setPositionDimensions({})
     const polygonBaseSize = fabricObject._getNonTransformedDimensions()
-    const newX =
-      (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x) /
-      polygonBaseSize.x
-    const newY =
-      (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y) /
-      polygonBaseSize.y
+    const newX = (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x) / polygonBaseSize.x
+    const newY = (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y) / polygonBaseSize.y
     fabricObject.setPositionByOrigin(absolutePoint, newX + 0.5, newY + 0.5)
     return actionPerformed
   }
@@ -97,18 +80,17 @@ function Edit (canvas) {
   const fabricPolygon = getPolygon(canvas)
 
   if (isRealValue(fabricPolygon)) {
-    // console.log("instance of Polygon:", fabricPolygon instanceof fabric.Polygon);
     canvas.setActiveObject(fabricPolygon)
     fabricPolygon.edit = !fabricPolygon.edit
+
     const cornerColor = getACornerColorThatShowsUp(fabricPolygon.stroke)
+
     if (fabricPolygon.edit) {
       const lastControl = fabricPolygon.points.length - 1
       fabricPolygon.cornerStyle = 'circle'
       fabricPolygon.cornerColor = cornerColor
-
-      // accumulator, next item, index
-      const reduceFun = function (acc, point, index) {
-        // Create a control object for each polygon point
+      // Create one new control for each polygon point
+      fabricPolygon.controls = fabricPolygon.points.reduce(function (acc, point, index) {
         acc['p' + index] = new fabric.Control({
           positionHandler: polygonPositionHandler,
           actionHandler: anchorWrapper(index > 0 ? index - 1 : lastControl, actionHandler),
@@ -116,15 +98,10 @@ function Edit (canvas) {
           pointIndex: index
         })
         return acc
-      }
-
-      // Create a control buddy [hashtable]. Point p0 = corresponding Control object.
-      // function, initial value
-      fabricPolygon.controls = fabricPolygon.points.reduce(reduceFun, {})
+      }, {})
     } else {
       fabricPolygon.cornerColor = cornerColor
       fabricPolygon.cornerStyle = 'rect'
-      // Default controls:
       fabricPolygon.controls = fabric.Object.prototype.controls
     }
     fabricPolygon.hasBorders = !fabricPolygon.edit
