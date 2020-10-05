9ca2911e6774b08b0a791af00f018b1096ca1d8b
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-05 11:49:48 -0400

    clearer

--- js/gridOverlay.js
+++ js/gridOverlay.js
@@ -1,128 +1,132 @@
-function gridOverlay (idx, viewer, overlay) {
-  const canvas = overlay.fabricCanvas()
-  const cellX = []
-  const cellY = []
+function gridOverlay (idx, overlay) {
   const cellSize = 50
-  let gridAdded = false
+
+  const gridProps = {
+    canvas: overlay.fabricCanvas(),
+    canvasWidth: Math.ceil(overlay.fabricCanvas().width),
+    canvasHeight: Math.ceil(overlay.fabricCanvas().height),
+    cellWidth: cellSize,
+    cellHeight: cellSize,
+    color: 'red',
+    cellX: [],
+    cellY: [],
+    gridAdded: false
+  }
 
   const btnGrid = document.getElementById('btnGrid' + idx)
   btnGrid.addEventListener('click', function () {
-    gridHandler(this)
+    gridHandler(this, gridProps)
   })
 
   const btnGridMarker = document.getElementById('btnGridMarker' + idx)
   btnGridMarker.addEventListener('click', function () {
-    markerHandler(this)
+    markerHandler(this, gridProps)
   })
+}
 
-  function gridHandler (button) {
-    toggleButtonHighlight(button)
+function gridHandler (button, gridProps) {
+  toggleButtonHighlight(button)
 
-    if (buttonIsOn(button)) {
-      const gridProperties = {
-        canvasWidth: Math.ceil(canvas.width),
-        canvasHeight: Math.ceil(canvas.height),
-        cellWidth: cellSize,
-        cellHeight: cellSize,
-        color: 'red'
-      }
-      turnGridOn(gridProperties)
-      gridAdded = true
-      button.innerHTML = '<i class="fa fa-border-all"></i> Remove grid'
-    }
-
-    if (!buttonIsOn(button)) {
-      turnGridOff()
-      gridAdded = false
-      button.innerHTML = '<i class="fa fa-border-all"></i> Draw grid'
-    }
+  if (buttonIsOn(button)) {
+    turnGridOn(gridProps)
+    gridProps.gridAdded = true
+    button.innerHTML = '<i class="fa fa-border-all"></i> Remove grid'
   }
 
-  function buttonIsOn (button) {
-    return button.classList.contains('btnOn')
+  if (!buttonIsOn(button)) {
+    turnGridOff(gridProps)
+    gridProps.gridAdded = false
+    button.innerHTML = '<i class="fa fa-border-all"></i> Draw grid'
   }
+}
 
-  function turnGridOff () {
-    const r = canvas.getObjects('line')
-    for (let i = 0; i < r.length; i++) {
-      canvas.remove(r[i])
-    }
+function buttonIsOn (button) {
+  return button.classList.contains('btnOn')
+}
+
+function turnGridOff (gridProps) {
+  const r = gridProps.canvas.getObjects('line')
+  for (let i = 0; i < r.length; i++) {
+    gridProps.canvas.remove(r[i])
   }
+}
 
-  function turnGridOn (gridProps) {
-    const lineProps = { stroke: gridProps.color, strokeWidth: 2, selectable: false }
+function turnGridOn (gridProps) {
+  const lineProps = { stroke: gridProps.color, strokeWidth: 2, selectable: false }
 
-    createHorizontalLines(gridProps, lineProps)
+  createHorizontalLines(gridProps, lineProps)
 
-    createVerticalLines(gridProps, lineProps)
+  createVerticalLines(gridProps, lineProps)
 
-    canvas.renderAll()
-    gridAdded = true
+  gridProps.canvas.renderAll()
+  gridProps.gridAdded = true
+}
+
+function createHorizontalLines (gridProps, lineProps) {
+  for (let y = 0; y < Math.ceil(gridProps.canvasHeight / gridProps.cellHeight); ++y) {
+    gridProps.canvas.add(new fabric.Line([0, y * gridProps.cellHeight, gridProps.canvasWidth, y * gridProps.cellHeight], lineProps))
+    gridProps.cellY[y + 1] = y * gridProps.cellHeight // and keep track of the y cells
   }
+}
 
-  function createHorizontalLines (gridProps, lineProps) {
-    for (let y = 0; y < Math.ceil(gridProps.canvasHeight / gridProps.cellHeight); ++y) {
-      canvas.add(new fabric.Line([0, y * gridProps.cellHeight, gridProps.canvasWidth, y * gridProps.cellHeight], lineProps))
-      cellY[y + 1] = y * gridProps.cellHeight // and keep track of the y cells
-    }
+function createVerticalLines (gridProps, lineProps) {
+  for (let x = 0; x < Math.ceil(gridProps.canvasWidth / gridProps.cellWidth); ++x) {
+    gridProps.canvas.add(new fabric.Line([x * gridProps.cellWidth, 0, x * gridProps.cellWidth, gridProps.canvasHeight], lineProps))
+    gridProps.cellX[x + 1] = x * gridProps.cellWidth // and keep track of the x cells
   }
+}
 
-  function createVerticalLines (gridProps, lineProps) {
-    for (let x = 0; x < Math.ceil(gridProps.canvasWidth / gridProps.cellWidth); ++x) {
-      canvas.add(new fabric.Line([x * gridProps.cellWidth, 0, x * gridProps.cellWidth, gridProps.canvasHeight], lineProps))
-      cellX[x + 1] = x * gridProps.cellWidth // and keep track of the x cells
-    }
-  }
+function mouseCoords (pointerEvent, gridProps) {
+  const mousePosition = getMousePosition(pointerEvent, gridProps)
+  const cellPosition = getCellPosition(mousePosition)
 
-  function mouseCoords (options) {
-    const mousePosition = getMousePosition(options)
-    const cellPosition = getCellPosition(mousePosition)
+  fillInGrid(cellPosition, gridProps)
+}
 
-    fillInGrid(cellPosition)
-  }
+function fillInGrid (cellPosition, gridProps) {
+  const rect = new fabric.Rect({
+    left: gridProps.cellX[cellPosition.x],
+    top: gridProps.cellY[cellPosition.y],
+    fill: 'red',
+    width: gridProps.cellWidth,
+    height: gridProps.cellHeight,
+    opacity: 0.5,
+    selectable: false
+  })
+  gridProps.canvas.add(rect)
+}
 
-  function fillInGrid (cellPosition) {
-    const rect = new fabric.Rect({
-      left: cellX[cellPosition.x],
-      top: cellY[cellPosition.y],
-      fill: 'red',
-      width: cellSize,
-      height: cellSize,
-      opacity: 0.5,
-      selectable: false
+function getMousePosition (pointerEvent, gridProps) {
+  const pointer = gridProps.canvas.getPointer(pointerEvent.e)
+  const positionX = pointer.x / gridProps.cellWidth
+  const positionY = pointer.y / gridProps.cellHeight
+  return { x: positionX, y: positionY }
+}
+
+function getCellPosition (mousePosition) {
+  const positionX = Math.ceil(mousePosition.x + 0.001)
+  const positionY = Math.ceil(mousePosition.y + 0.001)
+  return { x: positionX, y: positionY }
+}
+
+function markerHandler (button, gridProps) {
+  toggleButtonHighlight(button)
+
+  if (!buttonIsOn(button)) {
+    // Done marking; remove mouse:move listener because we use it for other things.
+    gridProps.canvas.off('mouse:move', function (event) {
+      mouseCoords(event, gridProps)
     })
-    canvas.add(rect)
-  }
-
-  function getMousePosition (options) {
-    console.log(options)
-    const pointer = canvas.getPointer(options.e)
-    const positionX = pointer.x / cellSize
-    const positionY = pointer.y / cellSize
-    return { x: positionX, y: positionY }
-  }
-
-  function getCellPosition (mousePosition) {
-    const positionX = Math.ceil(mousePosition.x + 0.001)
-    const positionY = Math.ceil(mousePosition.y + 0.001)
-    return { x: positionX, y: positionY }
-  }
-
-  function markerHandler (button) {
-    toggleButtonHighlight(button)
-
-    if (!buttonIsOn(button)) {
-      // Done marking; remove mouse:move listener because we use it for other things.
-      canvas.off('mouse:move', mouseCoords)
-      btnGridMarker.innerHTML = '<i class="fa fa-paint-brush"></i> Mark grid'
+    button.innerHTML = '<i class="fa fa-paint-brush"></i> Mark grid'
+  } else {
+    if (gridProps.gridAdded) {
+      gridProps.canvas.on('mouse:move', function (event) {
+        mouseCoords(event, gridProps)
+      })
+      button.innerHTML = '<i class="fa fa-paint-brush"></i> Done marking'
     } else {
-      if (gridAdded) {
-        canvas.on('mouse:move', mouseCoords)
-        btnGridMarker.innerHTML = '<i class="fa fa-paint-brush"></i> Done marking'
-      } else {
-        toggleButtonHighlight(btnGridMarker) // turn it back off; we're not letting them do this
-        alertMessage('Please draw a grid first.')
-      }
+      toggleButtonHighlight(button) // turn it back off; we're not letting them do this
+      alertMessage('Please draw a grid first.')
     }
   }
 }
