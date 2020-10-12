85ec4889ca658a597b2e0767267cd9f5f5abcf4e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-12 14:51:16 -0400

    added isEmpty

--- js/commonFunctions.js
+++ js/commonFunctions.js
@@ -19,6 +19,24 @@ function isRealValue (obj) {
   return obj !== null && obj !== 'undefined'
 }
 
+function isEmpty (value) {
+  const isEmptyObject = function (a) {
+    if (typeof a.length === 'undefined') { // it's an Object, not an Array
+      const hasNonempty = Object.keys(a).some(function nonEmpty (element) {
+        return !isEmpty(a[element])
+      })
+      return hasNonempty ? false : isEmptyObject(Object.keys(a))
+    }
+
+    return !a.some(function nonEmpty (element) { // check if array is really not empty as JS thinks
+      return !isEmpty(element) // at least one element should be non-empty
+    })
+  }
+  return (
+    value === false || typeof value === 'undefined' || value === null || (typeof value === 'object' && isEmptyObject(value))
+  )
+}
+
 function getAColorThatShowsUp (strokeColor) {
   function isBlueIsh () {
     return strokeColor.endsWith('ff')
