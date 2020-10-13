db79ef58f9a1ce00cf8ab639040bb1def3be3c38
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-13 13:41:13 -0400

    wip

--- js/packager.sh
+++ js/packager.sh
@@ -2,26 +2,24 @@
 
 rm ../dist/ultraviewer.min.js
 
-cat colorPicker.js commonFunctions.js dropDown.js checkOptions.js rangeSliders.js createPage.js pageSetup.js editPolygon.js drawPolygon.js gridOverlay.js mapMarker.js markupTools.js ImageViewer.js synchronizeViewers.js >../dist/ultraviewer.min.js
-#manual_minify
+function manual_minify() {
+  subl ../dist/ultraviewer.min.js vendor/ultraviewer.author.js
+  open https://javascript-minifier.com/
+}
 
 function foo() {
-  find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; >output
+  cat colorPicker.js commonFunctions.js dropDown.js checkOptions.js rangeSliders.js createPage.js pageSetup.js editPolygon.js drawPolygon.js gridOverlay.js mapMarker.js markupTools.js ImageViewer.js synchronizeViewers.js >../dist/ultraviewer.min.js
+  #find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; >output
   mv output ../dist/ultraviewer.min.js
   #minify
-  #manual_minify
+  manual_minify
 }
-#foo
+foo
 
 function minify() {
   $HOME/Documents/workspace/minify/bin/minifyjs ultraviewer.min.js >temp.js
   rm ultraviewer.min.js
   mv temp.js ultraviewer.min.js
-}
-
-function manual_minify() {
-  subl ../dist/ultraviewer.min.js vendor/ultraviewer.author.js
-  open https://javascript-minifier.com/
 }
 
 echo 'Done.'
