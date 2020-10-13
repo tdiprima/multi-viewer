382afacacea0774729ff9291fb169513c894a489
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-13 12:01:19 -0400

    wip

--- js/packager.sh
+++ js/packager.sh
@@ -2,9 +2,8 @@
 
 rm ../dist/ultraviewer.min.js
 
-cat colorPicker.js commonFunctions.js dropdown.js pageSetup.js editPolygon.js drawPolygon.js gridOverlay.js mapMarker.js markupTools.js ImageViewer.js synchronizeViewers.js > ../dist/ultraviewer.min.js
+cat colorPicker.js commonFunctions.js dropdown.js checkOptions.js rangeSliders.js createPage.js pageSetup.js editPolygon.js drawPolygon.js gridOverlay.js mapMarker.js markupTools.js ImageViewer.js synchronizeViewers.js >../dist/ultraviewer.min.js
 #manual_minify
-
 
 function foo() {
   find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; >output
@@ -13,12 +12,11 @@ function foo() {
   manual_minify
 }
 
-
 function minify() {
   $HOME/Documents/workspace/minify/bin/minifyjs ultraviewer.min.js >temp.js
-  rm ultraviewer.min.js; mv temp.js ultraviewer.min.js
+  rm ultraviewer.min.js
+  mv temp.js ultraviewer.min.js
 }
-
 
 function manual_minify() {
   subl ../dist/ultraviewer.min.js vendor/ultraviewer.author.js
