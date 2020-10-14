01f28f09d5c833cbb8c55231eebb574078379a2e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-14 12:42:35 -0400

    a better test

--- test/makeStructuresEqual.test.js
+++ test/makeStructuresEqual.test.js
@@ -1,50 +1,18 @@
 const makeStructuresEqual = require('../js/checkOptions')
 let result
 
-result = makeStructuresEqual({
-  foo: 1,
-  bar: 2,
-  zoo: 4,
-  bas: {
-    baz: 3
-  }
-}, {
-  foo: 1,
-  bar: 2,
-  bas: {
-    baz: 3
-  }
-})
-console.log(result)
+console.log('\nTest missing (1) field:')
+result = makeStructuresEqual({ foo: 1, bar: 2, zoo: 4, bas: { baz: 3 } }, { foo: 1, bar: 2, bas: { baz: 3 } })
+console.log('result:', result)
 
-result = makeStructuresEqual({
-  brand: 'brand 1',
-  navigation: { opacity: 0.2 }
-}, {
-  brand: 'brand 2',
-  navigation: { opacity: 0.5 }
-})
-console.log(result)
+console.log('\nTest equal structures; nothing to be done.')
+result = makeStructuresEqual({ brand: 'brand 1', navigation: { opacity: 0.2 } }, { brand: 'brand 2', navigation: { opacity: 0.5 } })
+console.log('result:', result)
 
-result = makeStructuresEqual({
-  filterOn: true,
-  slidersOn: true,
-  toolbarOn: true,
-  paintbrushColor: '#0ff',
-  viewerOpts: {
-    showFullPageControl: true,
-    showHomeControl: true,
-    showZoomControl: true
-  }
-}, {
-  filterOn: true,
-  slidersOn: true,
-  toolbarOn: true,
-  viewerOpts: {
-    showZoomControl: true
-  }
-})
-console.log(result)
+console.log('\nTest null:')
+result = makeStructuresEqual({ foo: 1 }, null)
+console.log('result:', result)
 
+console.log('\nTest empty:')
 result = makeStructuresEqual({ foo: 1 }, {})
-console.log(result)
+console.log('result:', result)
