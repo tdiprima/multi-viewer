044d0285cdb6c73a70eb39a27a8056db0adcf4bd
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-26 11:25:13 -0400

    window.location

--- mugshots/app.js
+++ mugshots/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // Image service Loris
   const infoUrl = 'https://iiif.princeton.edu/loris/iiif/2/pudl0001%2F4609321%2Fs42%2F00000001.jp2'
   // Image service SBU
-  // const infoUrl = 'https://quip.bmi.stonybrook.edu/iiif/?iiif=/tcgaseg/tcgaimages/blca/TCGA-2F-A9KO-01Z-00-DX1.195576CF-B739-4BD9-B15B-4A70AE287D3E.svs'
+  // const infoUrl = window.location.origin + '/iiif/?iiif=/tcgaseg/tcgaimages/blca/TCGA-2F-A9KO-01Z-00-DX1.195576CF-B739-4BD9-B15B-4A70AE287D3E.svs'
 
   const thumbnailSize = 256
   const scrollerLength = 5
