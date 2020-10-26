2ba1821de506ea7b3785631d48490b4d9184cadc
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-26 11:14:46 -0400

    testing

---
+++ test/checkBrowsers.test.js
@@ -0,0 +1,19 @@
+// const happy = require('selenium-webdriver')
+// console.log(happy)
+
+const { Builder, By, Key } = require('selenium-webdriver')
+
+async function openIt (url, browser) {
+  const driver = await new Builder().forBrowser(browser).build()
+
+  await driver.get(url)
+
+  await driver.findElement(By.name('q')).sendKeys('selenium', Key.RETURN)
+}
+
+// Not committing the real url ;)
+const url = 'https://google.com'
+
+// Test it in Firefox and Chrome
+openIt(url, 'firefox')
+openIt(url, 'chrome')
