34d72c6c7cc2099e8b53b6c1af241b981df96456
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-28 16:46:21 -0400

    Good.

--- mugshots/app2.js
+++ mugshots/app2.js
@@ -20,62 +20,57 @@ let oly = viewer.fabricjsOverlay({
 let canvas = oly.fabricCanvas()
 
 let image1
-// let size1 = 256 // can see from outer space, but we need smaller
-let size1 = 128
-let imageTL
-let imageBR
-let center
+let size = 256 // In image coordinates
+let topLeftImg
+let botRightImg
+let centerImg
+let imageRect
 
 // DRAW RECT, ZOOM, and GET MUG
 function drawRect() {
 
-  let windowTL = vpt.imageToWindowCoordinates(imageTL)
-  let windowBR = vpt.imageToWindowCoordinates(imageBR)
-  // let windowTL = vpt.imageToViewerElementCoordinates(imageTL)
-  // let windowBR = vpt.imageToViewerElementCoordinates(imageBR)
-  console.log('windowTL', windowTL)
-  console.log('windowBR', windowBR)
+  // CONVERT
+  // imageToWindowCoordinates
+  // imageToViewerElementCoordinates
+  let vptRect = vpt.imageToViewportRectangle(imageRect)
+  let webRect = vpt.viewportToViewerElementRectangle(vptRect)
+  console.log('webRect', webRect)
 
   let rect = new fabric.Rect({
     stroke: '#8a00ff',
     strokeWidth: 1,
     fill: '',
-    left: windowTL.x,
-    top: windowTL.y,
-    width: windowBR.x - windowTL.x,
-    height: windowBR.y - windowTL.y
+    left: webRect.x,
+    top: webRect.y,
+    width: webRect.width,
+    height: webRect.height
   })
   canvas.add(rect)
   canvas.renderAll()
 
-  // ZOOM
+  // PAN, ZOOM
+  vpt.panTo(vptRect.getCenter())
+  vpt.zoomTo(vpt.getMaxZoom())
 
-  // let point = vpt.imageToViewportCoordinates(center) // VE
-  // vpt.panTo(point)
-  // vpt.zoomTo(vpt.getMaxZoom())
+  // GET MUG
 
-  // // GET MUG
-  // let rect1 = new OpenSeadragon.Rect(rect.left, rect.top, rect.width, rect.height)
-  // let vptRect = vpt.viewerElementToViewportRectangle(rect1)
-  // let imgRect = vpt.viewportToImageRectangle(vptRect)
-  // console.log('img', imgRect)
-  //
-  // const infoUrl = window.location.origin + '/iiif/?iiif=/tcgaseg/tcgaimages/blca/TCGA-2F-A9KO-01Z-00-DX1.195576CF-B739-4BD9-B15B-4A70AE287D3E.svs'
-  // const mugSize = '256,'
-  // const rotation = '0'
-  // const quality = 'default'
-  // const format = 'jpg'
-  //
-  // let url = infoUrl + '/' +
-  //   Math.ceil(imgRect.x) + ',' +
-  //   Math.ceil(imgRect.y) + ',' +
-  //   Math.ceil(imgRect.width) + ',' +
-  //   Math.ceil(imgRect.height) + '/' +
-  //   mugSize + '/' +
-  //   rotation + '/' +
-  //   quality + '.' + format
-  //
-  // console.log(url)
+  // This info url is temporary - we have it already.
+  const infoUrl = window.location.origin + '/iiif/?iiif=/tcgaseg/tcgaimages/blca/TCGA-2F-A9KO-01Z-00-DX1.195576CF-B739-4BD9-B15B-4A70AE287D3E.svs'
+  const mugSize = '256,'
+  const rotation = '0'
+  const quality = 'default'
+  const format = 'jpg'
+
+  let url = infoUrl + '/' +
+    Math.ceil(imageRect.x) + ',' +
+    Math.ceil(imageRect.y) + ',' +
+    Math.ceil(imageRect.width) + ',' +
+    Math.ceil(imageRect.height) + '/' +
+    mugSize + '/' +
+    rotation + '/' +
+    quality + '.' + format
+
+  console.log(url)
 
 }
 
@@ -83,26 +78,38 @@ function getImage1() {
   // I already have this data
   image1 = viewer.world.getItemAt(0)
   let imgDimensions = image1.source.dimensions
-  console.log('imgDimensions', imgDimensions)
 
-  // Get center of image
-  let x = imgDimensions.x / 2
-  let y = imgDimensions.y / 2
-  center = new OpenSeadragon.Point(x, y)
-  console.log('center', center)
+  // Center of image
+  centerImg = getCenter(imgDimensions)
 
-  // Get center
-  let size = size1 / 2
-  // Shift upper-left of box by 'size' amount
-  x = x - size
-  y = y - size
-
-  // Just guarantee that we have whole numbers
-  imageTL = new OpenSeadragon.Point(Math.ceil(x), Math.ceil(y))
-  imageBR = new OpenSeadragon.Point(Math.ceil(x) + size1, Math.ceil(y) + size1)
-
-  console.log('imageTL', imageTL)
-  console.log('imageBR', imageBR)
+  // Top left of bounding box (for mug)
+  topLeftImg = shiftPoint(centerImg, size)
+  botRightImg = new OpenSeadragon.Point(topLeftImg.x + size, topLeftImg.y + size)
+  imageRect = new OpenSeadragon.Rect(topLeftImg.x, topLeftImg.y, size, size)
 
 }
 
+function getCenter(dims) {
+  // Get center of image
+  let x = dims.x / 2
+  let y = dims.y / 2
+
+  return new OpenSeadragon.Point(x, y)
+
+}
+
+function shiftPoint(centerPoint, size) {
+  // Half
+  const size1 = size / 2
+
+  // Shift upper-left of by 'size' amount
+  const x = centerPoint.x - size1
+  const y = centerPoint.y - size1
+
+  // Make sure we have whole numbers
+  return new OpenSeadragon.Point(Math.ceil(x), Math.ceil(y))
+}
+
+function convertToViewport(point) {
+  return vpt.viewerElementToViewportCoordinates(point)
+}
