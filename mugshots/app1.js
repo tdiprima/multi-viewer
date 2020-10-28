e5f3331592a680098488a450bd8167b3f4531b17
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-28 15:27:07 -0400

    More tests, clear explanations

--- mugshots/app1.js
+++ mugshots/app1.js
@@ -1,5 +1,6 @@
 // STARTING WITH WINDOW COORDINATES
-// let myTiles = 'https://openseadragon.github.io/example-images/duomo/duomo.dzi'
+// TODO: myTiles is temporary; we have it already.
+let myTiles = 'https://openseadragon.github.io/example-images/duomo/duomo.dzi'
 let divId = 'osd-placeholder'
 
 let viewer = OpenSeadragon({
@@ -19,19 +20,17 @@ let oly = viewer.fabricjsOverlay({
 let canvas = oly.fabricCanvas()
 
 // Get center of div
-let webCenterPt = getCenter(document.getElementById(divId))
-console.log('center of div', webCenterPt)
+let centerWeb = getCenter(document.getElementById(divId))
+console.log('center', centerWeb)
 
 let size = 24
-let topLeftWeb = shiftPoint(webCenterPt, size)
-console.log('draw rect', topLeftWeb)
+let topLeftWeb = shiftPoint(centerWeb, size)
+console.log('top left', topLeftWeb)
 console.log('size', size)
 // let botRightWeb = new OpenSeadragon.Point(x + size, y + size)
 
 // DRAW RECT, ZOOM, and GET MUG
 function drawRect() {
-
-  // TODO: I'm zoomed to the center, but the box is not dead center(?)
 
   let rect = new fabric.Rect({
     stroke: '#8a00ff',
@@ -45,19 +44,19 @@ function drawRect() {
   canvas.add(rect)
   canvas.renderAll()
 
-  // ZOOM
-
-  let point = vpt.viewerElementToViewportCoordinates(webCenterPt) // VE
-  vpt.panTo(point)
+  // CONVERT
+  // let point = convertToViewport(centerWeb) // Try this.
+  let rect1 = new OpenSeadragon.Rect(rect.left, rect.top, rect.width, rect.height) // Try this.
+  let vptRect = vpt.viewerElementToViewportRectangle(rect1)
+  // PAN, ZOOM
+  vpt.panTo(vptRect.getCenter())
   vpt.zoomTo(vpt.getMaxZoom())
-  console.log('zoom to center', webCenterPt)
 
   // GET MUG
-  let rect1 = new OpenSeadragon.Rect(rect.left, rect.top, rect.width, rect.height)
-  let vptRect = vpt.viewerElementToViewportRectangle(rect1)
   let imgRect = vpt.viewportToImageRectangle(vptRect)
   console.log('get this mug', imgRect)
 
+  // TODO: this info url is temporary - we have it already.
   const infoUrl = window.location.origin + '/iiif/?iiif=/tcgaseg/tcgaimages/blca/TCGA-2F-A9KO-01Z-00-DX1.195576CF-B739-4BD9-B15B-4A70AE287D3E.svs'
   const mugSize = '256,'
   const rotation = '0'
@@ -78,22 +77,28 @@ function drawRect() {
 }
 
 function getCenter(div) {
-  let x, y
-  // 1. Try client width, height
-  // x = div.clientWidth / 2
-  // y = div.clientHeight / 2
+  let w, h
+  // 1. Try: Get plain old dimensions of div
+  w = div.clientWidth
+  h = div.clientHeight
+  console.log('client w,h', w, h)
 
-  // 2. Try offsetWidth
-  // x = div.offsetWidth / 2
-  // y = div.offsetHeight / 2
+  // 2. Try: Get div with padding
+  // w = div.offsetWidth
+  // h = div.offsetHeight
+  // console.log('with padding w,h', w, h)
 
-  x = div.offsetWidth
-  y = div.offsetHeight
-  console.log(x, y)
+  // This is what we would return:
+  // return new OpenSeadragon.Point(w / 2, h / 2)
 
-  // 3. Try this method
+  // 3. Try: Offset with parent element
   let offPoint = getOffset(div)
-  return new OpenSeadragon.Point((x - offPoint.x) / 2, (y - offPoint.y) / 2)
+  let ww = w + offPoint.x
+  let hh = h - offPoint.y
+  console.log('offset w,h', ww, hh)
+
+  // Using offset, this is what we would return:
+  return new OpenSeadragon.Point(ww / 2, hh / 2)
 }
 
 function getOffset(someElement) {
@@ -102,9 +107,8 @@ function getOffset(someElement) {
     return m_getOffset(element.offsetParent, horizontal) + (horizontal ? element.offsetLeft : element.offsetTop);
   }
 
-  // calling
   let X = m_getOffset(someElement);
-  let Y = m_getOffset(someElement, true);
+  let Y = m_getOffset(someElement, true)
 
   return new OpenSeadragon.Point(X, Y)
 }
@@ -116,4 +120,13 @@ function shiftPoint(centerPoint, size) {
   const x = centerPoint.x - size1
   const y = centerPoint.y - size1
   return new OpenSeadragon.Point(x, y)
+}
+
+function convertToViewport(point) {
+  return vpt.viewerElementToViewportCoordinates(point)
+
+  // What if we did it like this?
+  // let image = vpt.windowToImageCoordinates(point)
+  // return vpt.imageToViewportCoordinates(image)
+
 }
