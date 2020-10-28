2fd35e50b4214f38dc92a07be2bb1cb673c3cb11
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-10-28 13:50:13 -0400

    better

--- mugshots/app1.js
+++ mugshots/app1.js
@@ -24,35 +24,15 @@ let webCenterPt = new OpenSeadragon.Point(x, y)
 console.log('webCenterPt', webCenterPt)
 
 let size1 = 30
-// I want that webCenterPt to be the webCenterPt of this box that I'm gonna draw
-// What's the size of the box?
+// Get center
 let size = size1 / 2 // webCenterPt
-// So that means shift upper-left of box by 'size' amount
-// We're still working in web pixels
+// Shift upper-left of box by 'size' amount
 x = x - size
 y = y - size
 
 // NOW WE NEED IMAGE COORDINATES, x, y, w, h, FOR THAT BOX
 let upLeftWeb = new OpenSeadragon.Point(x, y)
 let botRightWeb = new OpenSeadragon.Point(x + size, y + size)
-
-// USING POINT FROM PIXEL
-// let upLeftVpt = viewer.viewport.pointFromPixel(upLeftWeb)
-// let botRightVpt = viewer.viewport.pointFromPixel(botRightWeb)
-// let upLeftImg = viewer.viewport.viewportToImageCoordinates(upLeftVpt)
-// let botRightImg = viewer.viewport.viewportToImageCoordinates(botRightVpt)
-
-// USING WINDOW TO IMAGE
-let upLeftImg = viewer.viewport.windowToImageCoordinates(upLeftWeb)
-let botRightImg = viewer.viewport.windowToImageCoordinates(botRightWeb)
-
-let imageRect = {
-  x: upLeftImg.x,
-  y: upLeftImg.y,
-  w: botRightImg.x - upLeftImg.x,
-  h: botRightImg.y - upLeftImg.y
-}
-console.log('imageRect', imageRect)
 
 // DRAW RECT, ZOOM, and GET MUG
 function drawRect() {
@@ -71,18 +51,16 @@ function drawRect() {
 
   // ZOOM
 
-  // Try them both!
   let point = vpt.viewerElementToViewportCoordinates(webCenterPt) // VE
-
-  let point1 = vpt.windowToImageCoordinates(webCenterPt) // WI
-  point1 = vpt.imageToViewportCoordinates(point1) // IV
-
-  console.log(point + ' versus ' + point1)
-
   vpt.panTo(point)
   vpt.zoomTo(vpt.getMaxZoom())
 
   // GET MUG
+  let rect1 = new OpenSeadragon.Rect(rect.left, rect.top, rect.width, rect.height)
+  let vptRect = vpt.viewerElementToViewportRectangle(rect1)
+  let imgRect = vpt.viewportToImageRectangle(vptRect)
+  console.log('img', imgRect)
+
   const infoUrl = window.location.origin + '/iiif/?iiif=/tcgaseg/tcgaimages/blca/TCGA-2F-A9KO-01Z-00-DX1.195576CF-B739-4BD9-B15B-4A70AE287D3E.svs'
   const mugSize = '256,'
   const rotation = '0'
@@ -90,10 +68,10 @@ function drawRect() {
   const format = 'jpg'
 
   let url = infoUrl + '/' +
-    Math.round(imageRect.x) + ',' +
-    Math.round(imageRect.y) + ',' +
-    Math.round(imageRect.w) + ',' +
-    Math.round(imageRect.h) + '/' +
+    Math.ceil(imgRect.x) + ',' +
+    Math.ceil(imgRect.y) + ',' +
+    Math.ceil(imgRect.width) + ',' +
+    Math.ceil(imgRect.height) + '/' +
     mugSize + '/' +
     rotation + '/' +
     quality + '.' + format
