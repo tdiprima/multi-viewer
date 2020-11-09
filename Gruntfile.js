c2427b54d479881c383bbdeea7fd276bc50d9abf
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-09 17:10:19 -0500

    replaced packager.sh with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    concat: {
+      js: {
+        // src: ['src/**/*.js'],
+        src: ['js/*.js'],
+        dest: 'build/js/ultraviewer.js'
+      }
+    },
+    watch: {
+      js: {
+        files: ['js/*.js'],
+        tasks: ['concat'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  })
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+}
--- js/vendor/ultraviewer.author.js
+++
@@ -1,3 +0,0 @@
-/*!
- *  ULTRAVIEWER 2.4.6 by @tdiprima - https://github.com/tdiprima/multi-viewer
- */
