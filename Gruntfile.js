d7df31f54d0a0c63d9960ccc565b0e14d8eda5f2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-09 17:34:55 -0500

    uglify fails :(

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,15 @@ module.exports = function (grunt) {
         src: ['js/*.js'],
         dest: 'build/js/ultraviewer.js'
       }
-    },
-    uglify: {
-      my_target: {
-        files: {
-          'build/js/ultraviewer.min.js': ['js/*.js']
-          // 'build/js/ultraviewer.min.js': ['build/js/ultraviewer.js']
-        }
-      }
     }
+    // uglify: {
+    //   my_target: {
+    //     files: {
+    //       'build/js/ultraviewer.min.js': ['js/*.js']
+    //       // 'build/js/ultraviewer.min.js': ['build/js/ultraviewer.js']
+    //     }
+    //   }
+    // }
     //, watch: {
     //   js: {
     //     files: ['js/*.js'],
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
     // }
   })
   grunt.loadNpmTasks('grunt-contrib-concat')
-  grunt.loadNpmTasks('grunt-contrib-uglify')
+  // grunt.loadNpmTasks('grunt-contrib-uglify')
   // grunt.loadNpmTasks('grunt-contrib-watch')
   // grunt.registerTask('default', ['concat', 'watch'])
 }
