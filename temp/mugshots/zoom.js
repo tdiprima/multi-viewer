1a323c2fc7beac2831f1fb0315f37d949e609f84
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-10 10:37:53 -0500

    X

--- temp/mugshots/zoom.js
+++
@@ -1,6 +0,0 @@
-const panZoom = function (vpt, rectangle) {
-  vpt.panTo(rectangle.getCenter())
-  // console.log('zoomTo:', vpt.getMaxZoom())
-  // vpt.zoomTo(vpt.getMaxZoom()) // wsi 185
-  vpt.zoomTo(15) // 15 semi-works with WSI but not with Duomo
-}
