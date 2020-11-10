8c40a54b1ab638bb272eeba3ce35a296726fd9b2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-10 10:33:58 -0500

    concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,17 @@ module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+        src: ['src/*.js'],
+        dest: 'build/<%= pkg.name %>.js'
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -13,9 +24,10 @@ module.exports = function (grunt) {
     }
   })
 
-  // Load the plugin that provides the "uglify" task.
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-concat')
   grunt.loadNpmTasks('grunt-contrib-uglify')
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify'])
+  grunt.registerTask('default', ['concat'])
 }
