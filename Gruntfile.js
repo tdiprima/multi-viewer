c29b2a2dcfbe76f75d807dabe4c2d04bfe4a88bc
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-11-10 14:24:17 -0500

    minify css

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,21 @@ module.exports = function (grunt) {
         src: 'build/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'build/main.min.css': ['css/quad.css', 'css/main.css']
+        }
+      }
     }
   })
 
   // Load npm plugins to provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-concat')
   grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify'])
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin'])
 }
