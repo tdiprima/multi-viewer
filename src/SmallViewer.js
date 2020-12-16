506f86b1dd984497ba3aa9fa498bdb4449e150be
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-12-16 13:37:37 -0500

    Fixed nav btns when size below 500px

--- src/SmallViewer.js
+++ src/SmallViewer.js
@@ -13,19 +13,25 @@ const SmallViewer = function (viewerDivId, options, data) {
 
   let srcWidth, srcHeight
   let updateViewerSize = function (maxWidth, maxHeight) {
-    let fit = calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight)
-    let div = document.getElementById(viewerDivId)
+    const fit = calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight)
+    const div = document.getElementById(viewerDivId)
     div.style.width = fit.width + 'px'
     div.style.height = fit.height + 'px'
-    // TODO:
-    // if (maxWidth < 500) {
-    // }
+    if (maxWidth < 500) {
+      const buttons = viewer.navControl.childNodes
+      let i
+      for (i = 0; i < buttons.length; i++) {
+        if (buttons[i].innerHTML.indexOf('home') === -1) {
+          buttons[i].style.display = 'none'
+        }
+      }
+    }
   }
 
   viewer.addHandler('open', function () {
     srcHeight = (viewer.world.getItemAt(0).source.dimensions.y / window.devicePixelRatio)
     srcWidth = (viewer.world.getItemAt(0).source.dimensions.x / window.devicePixelRatio)
-    let div = document.getElementById(viewerDivId)
+    const div = document.getElementById(viewerDivId)
     console.log(div.clientWidth, div.clientHeight)
     updateViewerSize(div.clientWidth, div.clientHeight)
   })
@@ -41,23 +47,35 @@ const SmallViewer = function (viewerDivId, options, data) {
   // Public functions
   return {
     getImageDims: function () {
-      return {width: srcWidth, height: srcHeight}
+      return {
+        width: srcWidth,
+        height: srcHeight
+      }
     },
     getViewer: function () {
       return viewer
     },
     zoom: function () {
-      let center = new OpenSeadragon.Point(srcWidth / 2, srcHeight / 2)
+      const center = new OpenSeadragon.Point(srcWidth / 2, srcHeight / 2)
       console.log('center', center)
-      let point = viewer.viewport.imageToViewportCoordinates(center)
+      const point = viewer.viewport.imageToViewportCoordinates(center)
       viewer.viewport.panTo(new OpenSeadragon.Point(point.x, point.y))
       viewer.viewport.zoomTo(viewer.viewport.getMaxZoom())
     },
     updateViewerSize: function (maxWidth, maxHeight) {
-      let fit = calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight)
-      let div = document.getElementById(viewerDivId)
+      const fit = calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight)
+      const div = document.getElementById(viewerDivId)
       div.style.width = fit.width + 'px'
       div.style.height = fit.height + 'px'
+      if (maxWidth < 500) {
+        const buttons = viewer.navControl.childNodes
+        let i
+        for (i = 0; i < buttons.length; i++) {
+          if (buttons[i].innerHTML.indexOf('home') === -1) {
+            buttons[i].style.display = 'none'
+          }
+        }
+      }
     }
   }
 }
