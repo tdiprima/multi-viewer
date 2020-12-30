6fe73019568362e3a40159e1c9b2eed1437a7d8a
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-12-30 16:13:37 -0500

    wip

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -1,8 +1,8 @@
 class ImageViewer extends SmallViewer {
   constructor (viewerDivId, imageArray, opacityArray, sliderElements, numDivs, options) {
     super(viewerDivId, imageArray, opacityArray)
+    console.log(typeof sliderElements === 'undefined')
     let viewer1 = super.getViewer()
     console.log(viewer1)
   }
-
 }
