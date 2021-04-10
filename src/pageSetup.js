eb14462bc9e26cc6e494d2e3a35be1013499a4ad
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-04-10 09:13:38 -0400

    Moved event handler

--- src/pageSetup.js
+++ src/pageSetup.js
@@ -180,51 +180,6 @@ const pageSetup = function (divId, image, features, opacity, numViewers, rows, c
             dragElement(myDiv)
           })
 
-          function dragElement(elmnt) {
-            var pos1 = 0
-            var pos2 = 0
-            var pos3 = 0
-            var pos4 = 0
-      
-            if (document.getElementById(elmnt.id + 'Header')) {
-              // if present, the header is where you move the DIV from:
-              document.getElementById(elmnt.id + 'Header').onmousedown = dragMouseDown
-            } else {
-              // otherwise, move the DIV from anywhere inside the DIV:
-              elmnt.onmousedown = dragMouseDown
-            }
-      
-            function dragMouseDown(e) {
-              e = e || window.event
-              e.preventDefault()
-              // get the mouse cursor position at startup:
-              pos3 = e.clientX
-              pos4 = e.clientY
-              document.onmouseup = closeDragElement
-              // call a function whenever the cursor moves:
-              document.onmousemove = elementDrag
-            }
-      
-            function elementDrag(e) {
-              e = e || window.event
-              e.preventDefault()
-              // calculate the new cursor position:
-              pos1 = pos3 - e.clientX
-              pos2 = pos4 - e.clientY
-              pos3 = e.clientX
-              pos4 = e.clientY
-              // set the element's new position:
-              elmnt.style.top = (elmnt.offsetTop - pos2) + 'px'
-              elmnt.style.left = (elmnt.offsetLeft - pos1) + 'px'
-            }
-      
-            function closeDragElement() {
-              // stop moving when mouse button is released:
-              document.onmouseup = null
-              document.onmousemove = null
-            }
-          }
-
           // NEED TO PASS THESE TO VIEWER
           let sliderElements = []
           try {
