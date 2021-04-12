4058cfa668519b85e4ef6a75d8e6287b78ea4630
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-04-12 15:52:45 -0400

    check if undefined

--- src/imageFiltering.js
+++ src/imageFiltering.js
@@ -34,30 +34,26 @@ const imageFiltering = function () {
           try {
             // w x h: 256 x 256
             imgData = context.getImageData(0, 0, context.canvas.width, context.canvas.height)
-          }
-          catch (err) {
-            console.error('COLORIZE:', err.message)
-          }
-          const pixels = imgData.data
-          let i
-          // Run the filter on them
-          for (i = 0; i < pixels.length; i += 4) {
-            if (pixels[i + 3] === 255) {
-              // Alpha channel = 255 ("opaque"): nuclear material here.
-              pixels[i] = color.r
-              pixels[i + 1] = color.g
-              pixels[i + 2] = color.b
-              pixels[i + 3] = 255
-            } else {
-              // No nuclear material: set to transparent.
-              pixels[i + 3] = 0
+            if (typeof imgData !== undefined) {
+              const pixels = imgData.data
+              // Run the filter on them
+              let i
+              for (i = 0; i < pixels.length; i += 4) {
+                if (pixels[i + 3] === 255) {
+                  // Alpha channel = 255 ("opaque"): nuclear material here.
+                  pixels[i] = color.r
+                  pixels[i + 1] = color.g
+                  pixels[i + 2] = color.b
+                  pixels[i + 3] = 255
+                } else {
+                  // No nuclear material: set to transparent.
+                  pixels[i + 3] = 0
+                }
+              }
+              // Write the result back onto the canvas
+              context.putImageData(imgData, 0, 0)
+              callback()
             }
-          }
-          // Write the result back onto the canvas
-          try {
-            // console.log('imgData', imgData)
-            context.putImageData(imgData, 0, 0)
-            callback()
           }
           catch (err) {
             console.error('COLORIZE:', err.message)
@@ -68,7 +64,7 @@ const imageFiltering = function () {
       return filter
     },
     getFilter1: function () {
-      // WORK IN PROGRESS!!!
+      // WORK IN PROGRESS!
       let filter1 = {}
       filter1 = OpenSeadragon.Filters.GREYSCALE
       filter1.prototype.COLORLEVELS = function (placeholder) {
@@ -76,60 +72,55 @@ const imageFiltering = function () {
           let imgData
           try {
             imgData = context.getImageData(0, 0, context.canvas.width, context.canvas.height)
-          }
-          catch (err) {
-            console.error('COLORLEVELS:', err.message)
-          }
-          const pxl = imgData.data
-          let j
-          for (j = 0; j < pxl.length; j += 4) {
-            if (pxl[j + 3] === 255) {
-              // PRETEND!!!
-              var v = (pxl[j] + pxl[j + 1] + pxl[j + 2]) / 3 | 0
-              let rgba = levels(v)
-              pxl[j] = rgba.r
-              pxl[j + 1] = rgba.g
-              pxl[j + 2] = rgba.b
-              pxl[j + 3] = rgba.a
-            } else {
-              pxl[j + 3] = 0
-            }
-          }
-          function levels(val) {
-            if (val >= 0 && val <= 30) {
-              return { r: 255, g: 255, b: 255, a: 0 }
-            }
 
-            if (val > 30 && val <= 75) {
-              return { r: 135, g: 19, b: 172, a: 255 }
+            if (typeof imgData !== undefined) {
+              const pxl = imgData.data
+              let j
+              for (j = 0; j < pxl.length; j += 4) {
+                if (pxl[j + 3] === 255) {
+                  // var v = (pxl[j] + pxl[j + 1] + pxl[j + 2]) / 3 | 0
+                  let rgba = levels(pxl[j]) // r = g = b
+                  pxl[j] = rgba.r
+                  pxl[j + 1] = rgba.g
+                  pxl[j + 2] = rgba.b
+                  pxl[j + 3] = rgba.a
+                } else {
+                  pxl[j + 3] = 0
+                }
+              }
+              function levels(val) {
+                if (val >= 0 && val <= 30) {
+                  return { r: 255, g: 255, b: 255, a: 0 }
+                }
+
+                if (val > 30 && val <= 75) {
+                  return { r: 135, g: 19, b: 172, a: 255 }
+                }
+
+                if (val > 75 && val <= 100) {
+                  return { r: 0, g: 0, b: 255, a: 255 }
+                }
+
+                if (val > 100 && val <= 140) {
+                  return { r: 1, g: 185, b: 245, a: 255 }
+                }
+
+                if (val > 140 && val <= 170) {
+                  return { r: 255, g: 255, b: 0, a: 255 }
+                }
+
+                if (val > 170 && val <= 200) {
+                  return { r: 255, g: 153, b: 0, a: 255 }
+                }
+
+                if (val > 200) {
+                  return { r: 255, g: 0, b: 0, a: 255 }
+                }
+
+              }
+              context.putImageData(imgData, 0, 0)
+              callback()
             }
-
-            if (val > 75 && val <= 100) {
-              return { r: 0, g: 0, b: 255, a: 255 }
-            }
-
-            if (val > 100 && val <= 140) {
-              return { r: 1, g: 185, b: 245, a: 255 }
-            }
-
-            if (val > 140 && val <= 170) {
-              return { r: 255, g: 255, b: 0, a: 255 }
-            }
-
-            if (val > 170 && val <= 200) {
-              return { r: 255, g: 153, b: 0, a: 255 }
-            }
-
-            if (val > 200) {
-              return { r: 255, g: 0, b: 0, a: 255 }
-            }
-
-          }
-
-          try {
-            // console.log('imgData', imgData)
-            context.putImageData(imgData, 0, 0)
-            callback()
           }
           catch (err) {
             console.error('COLORLEVELS:', err.message)
