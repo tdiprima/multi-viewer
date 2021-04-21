5f6d584ad2f802d4e1bf6901f76aadd717681a91
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-04-21 16:02:08 -0400

    fix

--- src/imageFiltering.js
+++ src/imageFiltering.js
@@ -125,6 +125,92 @@ const imageFiltering = function () {
     })
   }
 
+  function createPopup(event, layersBtn, viewer) {
+    // Disable buttons
+    buttonToggle('#ccc', 'not-allowed')
+
+    // Highlight this button
+    layersBtn.style.color = '#0f0'
+    layersBtn.style.cursor = 'pointer'
+
+    // Main container
+    let colorPopup = document.createElement('div')
+    colorPopup.id = 'colorPopup'
+    colorPopup.classList.add('grid-container')
+    colorPopup.classList.add('colorPopup')
+
+    // Close button
+    let d = document.createElement('div')
+    d.className = 'popupHeader'
+    const img = document.createElement('img')
+    img.src = 'images/close_icon.png'
+    img.width = 25
+    img.height = 25
+    img.style.cssFloat = 'left'
+    d.appendChild(img)
+
+    // Remove div on click
+    img.addEventListener('click', function () {
+      layersBtn.style.color = '#000'
+      // Re-enable buttons
+      buttonToggle('#000', 'pointer')
+      this.parentNode.parentNode.remove()
+    })
+    colorPopup.appendChild(d)
+
+    // Header to drag around screen
+    const popupHeader = document.createElement('div')
+    popupHeader.className = 'popupHeader'
+    popupHeader.innerHTML = 'Color Levels'
+    colorPopup.appendChild(popupHeader)
+    let t = document.createElement('div')
+    t.className = 'popupHeader'
+    colorPopup.appendChild(t)
+
+    // CREATE USER INPUT PER COLOR
+    // Display colors and low/high values
+    // {color: "rgba(r, g, b, a)", hi: n, low: n}
+    colorRanges.forEach(function (cr, index) {
+      // COLOR DIV
+      let colorDiv = document.createElement('div')
+      colorDiv.style.backgroundColor = cr.color
+      colorDiv.style.width = '20px'
+      colorDiv.style.height = '20px'
+
+      // LOW
+      let lowDiv = document.createElement('div')
+      lowDiv.appendChild(createInput('low' + index, cr.low))
+
+      // HIGH
+      let hiDiv = document.createElement('div')
+      hiDiv.appendChild(createInput('hi' + index, cr.hi))
+
+      // ADD TO CONTAINER DIV
+      colorPopup.appendChild(colorDiv)
+      colorPopup.appendChild(lowDiv)
+      colorPopup.appendChild(hiDiv)
+    })
+
+    colorPopup.style.left = event.clientX + 'px'
+    colorPopup.style.top = event.clientY + 'px'
+
+    document.body.appendChild(colorPopup)
+
+    let imf1 = new imageFiltering()
+    let filter1 = imf1.getFilter1()
+    viewer.setFilterOptions({
+      filters: [{
+        items: viewer.world.getItemAt(1), // TODO: Layer #
+        processors: [
+          filter1.prototype.COLORLEVELS(colorRanges)
+        ]
+      }]
+    })
+
+    // Make the DIV element draggable:
+    dragElement(colorPopup)
+  }
+
   return {
     getFilter: function () {
       let filter = {}
@@ -231,96 +317,17 @@ const imageFiltering = function () {
       return filters.length
     },
     handleColorLevels: function (layersBtn, viewer) {
-      // COLOR RANGE POPUP
-      let colorPopup
-
       // Layers button clicked
       layersBtn.addEventListener('click', function (event) {
         event = event || window.event
 
-        // Disable buttons
-        buttonToggle('#ccc', 'not-allowed')
-
-        // Highlight this button
-        layersBtn.style.color = '#0f0'
-        layersBtn.style.cursor = 'pointer'
-
-        // Main container
-        colorPopup = document.createElement('div')
-        colorPopup.id = 'colorPopup'
-        colorPopup.classList.add('grid-container')
-        colorPopup.classList.add('colorPopup')
-
-        // Close button
-        let d = document.createElement('div')
-        d.className = 'popupHeader'
-        const img = document.createElement('img')
-        img.src = 'images/close_icon.png'
-        img.width = 25
-        img.height = 25
-        img.style.cssFloat = 'left'
-        d.appendChild(img)
-
-        // Remove div on click
-        img.addEventListener('click', function () {
-          layersBtn.style.color = '#000'
-          // Re-enable buttons
-          buttonToggle('#000', 'pointer')
-          this.parentNode.parentNode.remove()
-        })
-        colorPopup.appendChild(d)
-
-        // Header to drag around screen
-        const popupHeader = document.createElement('div')
-        popupHeader.className = 'popupHeader'
-        popupHeader.innerHTML = 'Color Levels'
-        colorPopup.appendChild(popupHeader)
-        let t = document.createElement('div')
-        t.className = 'popupHeader'
-        colorPopup.appendChild(t)
-
-        // CREATE USER INPUT PER COLOR
-        // Display colors and low/high values
-        // {color: "rgba(r, g, b, a)", hi: n, low: n}
-        colorRanges.forEach(function (cr, index) {
-          // COLOR DIV
-          let colorDiv = document.createElement('div')
-          colorDiv.style.backgroundColor = cr.color
-          colorDiv.style.width = '20px'
-          colorDiv.style.height = '20px'
-
-          // LOW
-          let lowDiv = document.createElement('div')
-          lowDiv.appendChild(createInput('low' + index, cr.low))
-
-          // HIGH
-          let hiDiv = document.createElement('div')
-          hiDiv.appendChild(createInput('hi' + index, cr.hi))
-
-          // ADD TO CONTAINER DIV
-          colorPopup.appendChild(colorDiv)
-          colorPopup.appendChild(lowDiv)
-          colorPopup.appendChild(hiDiv)
-        })
-
-        colorPopup.style.left = event.clientX + 'px'
-        colorPopup.style.top = event.clientY + 'px'
-
-        document.body.appendChild(colorPopup)
-
-        let imf1 = new imageFiltering()
-        let filter1 = imf1.getFilter1()
-        viewer.setFilterOptions({
-          filters: [{
-            items: viewer.world.getItemAt(1), // TODO: Layer #
-            processors: [
-              filter1.prototype.COLORLEVELS(colorRanges)
-            ]
-          }]
-        })
-
-        // Make the DIV element draggable:
-        dragElement(colorPopup)
+        // Let there be only one
+        let el = document.getElementById('colorPopup')
+        if (el) {
+          console.log('colorPopup')
+        } else {
+          createPopup(event, layersBtn, viewer)
+        }
       })
     },
     getColor: function (num) {
