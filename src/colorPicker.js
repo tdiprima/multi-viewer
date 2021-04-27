45a83debc7a42f88da57298f93e6a21a4670cadb
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-04-27 18:01:29 -0400

    :guide_dog: debug

--- src/colorPicker.js
+++ src/colorPicker.js
@@ -1,16 +1,31 @@
 // Utilizes: https://github.com/taufik-nurrohman/color-picker
 const colorPicker = function (inputElement) {
+  console.warn('colorPicker', inputElement)
+
   // Check
   if (!isRealValue(inputElement)) {
+    console.error('HERE')
     throw Error('colorPicker.js: Expected input argument, but received none.')
   }
 
   // Construct
   const picker = new CP(inputElement)
+  console.warn('CP', picker)
   picker.on('change', function (r, g, b, a) {
-    this.source.value = this.color(r, g, b, a)
-    this.source.innerHTML = this.color(r, g, b, a)
-    this.source.style.backgroundColor = this.color(r, g, b, a)
+    try {
+      console.warn('r, g, b, a', r, g, b, a)
+      this.source.value = this.color(r, g, b, a)
+
+      console.warn('this.source.value', this.source.value)
+      this.source.innerHTML = this.color(r, g, b, a)
+
+      console.warn('this.source.innerHTML', this.source.innerHTML)
+      this.source.style.backgroundColor = this.color(r, g, b, a)
+    }
+    catch (err) {
+      console.error('Caught!', err.message)
+    }
+
   })
 
   return picker
