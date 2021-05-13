f3dfe03d019bf9751e83caa8b79bba0f6e254bf2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-05-13 16:05:58 -0400

    temp

--- src/draggable.js
+++ src/draggable.js
@@ -1,8 +1,34 @@
-// Make the DIV element draggable:
-// dragElement(document.getElementById('mydiv'))
+function createDraggableDiv(id, title) {
+  let div = document.createElement('div')
+  div.id = id
 
-function dragElement (elmnt) {
-  let pos1 = 0; let pos2 = 0; let pos3 = 0; let pos4 = 0
+  // Include a header DIV with the same name as the draggable DIV, followed by "Header"
+  let divHead = document.createElement('div')
+  divHead.id = `${id}Header`
+  div.appendChild(divHead)
+
+  let img = document.createElement('img')
+  img.src = 'images/close_icon.png'
+  img.width = 25
+  img.height = 25
+  img.style.cssFloat = 'left'
+  divHead.appendChild(img)
+
+
+  // Ditto body
+  let divBody = document.createElement('div')
+  divBody.id = `${id}Body`
+  div.appendChild(divHead)
+
+  return div
+}
+
+// Make the DIV element draggable:
+function dragElement(elmnt) {
+  let pos1 = 0;
+  let pos2 = 0;
+  let pos3 = 0;
+  let pos4 = 0
   if (document.getElementById(elmnt.id + 'Header')) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + 'Header').onmousedown = dragMouseDown
@@ -12,7 +38,7 @@ function dragElement (elmnt) {
   }
 
   // Mousedown handler
-  function dragMouseDown (e) {
+  function dragMouseDown(e) {
     e = e || window.event
     e.preventDefault()
     // get the mouse cursor position at startup:
@@ -24,7 +50,7 @@ function dragElement (elmnt) {
   }
 
   // Mouse-move handler
-  function elementDrag (e) {
+  function elementDrag(e) {
     e = e || window.event
     e.preventDefault()
     // calculate the new cursor position:
@@ -38,7 +64,7 @@ function dragElement (elmnt) {
   }
 
   // Done handler
-  function closeDragElement () {
+  function closeDragElement() {
     // stop moving when mouse button is released:
     document.onmouseup = null
     document.onmousemove = null
