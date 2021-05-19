8418908676e3f417b810c1afdc5047a6f2357787
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-05-19 11:01:33 -0400

    str.hashCode()

--- src/commonFunctions.js
+++ src/commonFunctions.js
@@ -84,3 +84,15 @@ function makeId(length, prefix) {
   }
   return result
 }
+
+String.prototype.hashCode = function () {
+  let hash = 0
+  if (this.length === 0) return hash
+  let i, char;
+  for (i = 0; i < this.length; i++) {
+    char = this.charCodeAt(i)
+    hash = ((hash << 5) - hash) + char
+    hash = hash & hash // Convert to 32bit integer
+  }
+  return hash
+}
