efe3f4d37c929cdc36507a7a3b657361c71fd3ac
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-05-26 14:57:30 -0400

    n/a

--- test/synchronizeViewers.test.js
+++
@@ -1,7 +0,0 @@
-const synchronizeViewers = require('../src/synchronizeViewers')
-
-console.log('Test empty array')
-synchronizeViewers([])
-
-console.log('Test non-viewer array elements')
-synchronizeViewers(['a', 'b', 'c'])
