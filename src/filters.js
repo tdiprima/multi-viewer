67a2f644e903c55d24de66858a5614e132730374
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-07-07 11:03:37 -0400

    ID and syntax fixes
    Clarifying the naming convention for this function
    
    Fixed: multiline-ternary and no-mixed-operators lint

--- src/filters.js
+++ src/filters.js
@@ -1,20 +1,16 @@
-let filters = function (button, layer, layers, viewer) {
-  let div
-  if (isRealValue(button)) {
-    let id = makeId(5, 'filters')
-    let rect = button.getBoundingClientRect()
-    div = createDraggableDiv(id, 'Color Levels', rect.left, rect.top)
-    createUI(document.getElementById(`${id}Body`), layer, layers, viewer)
-  } else {
-    console.log('tba')
-  }
+const filters = function (button, layer, layers, viewer) {
+  const identifier = getRandomInt(100, 999)
+  const id = `filters${identifier}`
+  const rect = button.getBoundingClientRect()
+  const div = createDraggableDiv(id, 'Color Levels', rect.left, rect.top)
+  createUI(identifier, document.getElementById(`${id}Body`), layer, layers, viewer)
   return div
 }
 
 // SET LAYERS' COLOR FILTER
-let setFilter = function (layers, viewer) {
-  let itemCount = viewer.world.getItemCount()
-  let filterOpts = []
+const setFilter = function (layers, viewer) {
+  const itemCount = viewer.world.getItemCount()
+  const filterOpts = []
   for (let i = 0; i < itemCount; i++) {
     if (i > 0) {
       filterOpts.push({
@@ -32,7 +28,7 @@ let setFilter = function (layers, viewer) {
 }
 
 // COLOR RANGES UI
-let createUI = function (div, layer, layers, viewer) {
+const createUI = function (uniq, div, layer, layers, viewer) {
   const table = document.createElement('table')
   div.appendChild(table)
 
@@ -45,20 +41,19 @@ let createUI = function (div, layer, layers, viewer) {
     // Sort intervals in decreasing order of low value
     layer.colors.sort((a, b) => b.low - a.low)
 
-    const uniq = makeId(5) // create it outside of loop
     // Display a row of tools for each layer
     layer.colors.forEach(function (c, cIdx) {
       // calling it 'c' because 'color' is already taken
-      let tr = table.insertRow(-1)
+      const tr = table.insertRow(-1)
       table.appendChild(tr)
 
       let td = tr.insertCell(-1)
-      let colorCode = c.color
+      const colorCode = c.color
 
       // COLOR PICKER
-      let m = document.createElement('mark')
-      m.id = makeId(5, 'marker')
-      m.innerHTML = "#" + rgba2hex(colorCode)
+      const m = document.createElement('mark')
+      m.id = `marker${uniq}`
+      m.innerHTML = '#' + rgba2hex(colorCode)
       m.style.backgroundColor = colorCode
       td.appendChild(m)
 
@@ -82,25 +77,24 @@ let createUI = function (div, layer, layers, viewer) {
       // HIGH
       td = tr.insertCell(-1)
       td.appendChild(createNumericInput(`hi${uniq}${cIdx}`, uniq, layers, c, layer.colors, viewer))
-
     })
   }
-
 }
 
 function rgba2hex(orig) {
-  let a,
-    arr = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
-    alpha = (arr && arr[4] || "").trim(),
-    hex = arr ?
-      (arr[1] | 1 << 8).toString(16).slice(1) +
-      (arr[2] | 1 << 8).toString(16).slice(1) +
-      (arr[3] | 1 << 8).toString(16).slice(1) : orig;
+  let a
+  const arr = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i)
+  const alpha = ((arr && arr[4]) || '').trim()
+  let hex = arr ?
+    (arr[1] | 1 << 8).toString(16).slice(1) +
+    (arr[2] | 1 << 8).toString(16).slice(1) +
+    (arr[3] | 1 << 8).toString(16).slice(1)
+    : orig
 
-  if (alpha !== "") {
-    a = alpha;
+  if (alpha !== '') {
+    a = alpha
   } else {
-    a = 0x0;
+    a = 0x0
   }
   a = (a | 1 << 8).toString(16).slice(1)
   hex = hex + a
@@ -109,7 +103,7 @@ function rgba2hex(orig) {
 
 // USER INPUTS to set color threshold values
 function createNumericInput(id, uniq, layers, color, colors, viewer) {
-  let x = document.createElement('input')
+  const x = document.createElement('input')
   x.id = id
   x.setAttribute('type', 'number')
   x.min = '0'
@@ -125,7 +119,7 @@ function createNumericInput(id, uniq, layers, color, colors, viewer) {
 
   // 'input' fires for each alteration; a user can enter a number without exiting the field.
   x.addEventListener('input', function () {
-    let intVal = parseInt(this.value)
+    const intVal = parseInt(this.value)
 
     // If they set it to something outside of 0-255, reset it
     if (intVal > 255) this.value = '255'
@@ -143,7 +137,6 @@ function createNumericInput(id, uniq, layers, color, colors, viewer) {
 }
 
 function isIntersect(uniq, len) {
-
   // Clear all previous errors
   for (let i = 0; i < len; i++) {
     clearError(document.getElementById('low' + uniq + i), document.getElementById('hi' + uniq + i))
@@ -152,14 +145,14 @@ function isIntersect(uniq, len) {
   // Validation
   for (let i = 1; i < len; i++) {
     // If low of an interval, is less than the high value of the 'previous' (next) interval, then error
-    let low = document.getElementById('low' + uniq + (i - 1))
-    let high = document.getElementById('hi' + uniq + i)
+    const low = document.getElementById('low' + uniq + (i - 1))
+    const high = document.getElementById('hi' + uniq + i)
     if (parseInt(low.value) < parseInt(high.value)) {
       setError(low, high)
       return true
     }
     // If high ends up being less than its low, then error
-    let high1 = document.getElementById('hi' + uniq + (i - 1))
+    const high1 = document.getElementById('hi' + uniq + (i - 1))
     if (parseInt(high1.value) < parseInt(low.value)) {
       setError(low, high1)
       return true
@@ -188,14 +181,14 @@ let colorFilter = OpenSeadragon.Filters.GREYSCALE
 colorFilter.prototype.COLORLEVELS = data => (context, callback) => {
   if (context.canvas.width > 0 && context.canvas.height > 0) {
     // Read the canvas pixels
-    let imgData = context.getImageData(0, 0, context.canvas.width, context.canvas.height)
-    if (typeof imgData !== undefined) {
+    const imgData = context.getImageData(0, 0, context.canvas.width, context.canvas.height)
+    if (typeof imgData !== 'undefined') {
       try {
         const pxl = imgData.data // Uint8ClampedArray
         let j
         for (j = 0; j < pxl.length; j += 4) {
           if (pxl[j + 3] === 255) {
-            let rgba = levels(pxl[j], data) // r = g = b
+            const rgba = levels(pxl[j], data) // r = g = b
             if (typeof rgba === 'undefined') {
               console.warn('rgba undefined', pxl[j])
             }
@@ -217,9 +210,9 @@ colorFilter.prototype.COLORLEVELS = data => (context, callback) => {
           let i
           let retVal
           for (i = 0; i < _colors.length; i++) {
-            let low = _colors[i].low
-            let hi = _colors[i].hi
-            let color = _colors[i].color
+            const low = _colors[i].low
+            const hi = _colors[i].hi
+            const color = _colors[i].color
             if (value >= low && value <= hi) {
               retVal = parseColor(color)
             }
@@ -246,7 +239,6 @@ colorFilter.prototype.COLORLEVELS = data => (context, callback) => {
       } catch (err) {
         console.warn('3:', err.message)
       }
-
     } else {
       console.warn('imgData undefined')
     }
