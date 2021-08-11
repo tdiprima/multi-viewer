ab6f6452d80921a82041f7f6cefece0236af84b6
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-08-11 15:45:19 -0400

    comment

--- src/markupTools.js
+++ src/markupTools.js
@@ -1,7 +1,6 @@
 const markupTools = function (idx, viewer) {
-  const overlay = viewer.fabricjsOverlay({
-    scale: 1000
-  })
+  const overlay = viewer.fabricjsOverlay({ scale: 1000 })
+  // Note: pass the overlay to all the functions
   drawPolygon(document.getElementById('btnDraw' + idx), document.getElementById('mark' + idx), viewer, overlay)
   editPolygon(document.getElementById('btnEdit' + idx), overlay)
   gridOverlay(document.getElementById('btnGrid' + idx), document.getElementById('btnGridMarker' + idx), overlay)
