b5a71aa5471c0d20d8f6b4244e6064838b144863
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-08-17 13:37:27 -0400

    Update filters.js

--- src/filters.js
+++ src/filters.js
@@ -162,7 +162,6 @@ function createNumericInput(id, uniq, layers, colorLowHi, colors, viewer) {
     value: id.includes('low') ? colorLowHi.low.toString() : colorLowHi.hi.toString()
   })
 
-  // todo: validation
   x.addEventListener('change', function () {
     isIntersect(uniq, colors.length)
   })
@@ -185,7 +184,6 @@ function createNumericInput(id, uniq, layers, colorLowHi, colors, viewer) {
   return x
 }
 
-// todo: validation
 function isIntersect(uniq, len) {
   // Clear all previous errors
   for (let i = 0; i < len; i++) {
