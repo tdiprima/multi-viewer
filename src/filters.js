ce6c04a1425ed9e14e732d4b93f66b77b389cb7f
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-09-01 12:11:18 -0400

    useCapture

--- src/filters.js
+++ src/filters.js
@@ -31,7 +31,7 @@ function createUI(uniq, div, layerColors, layers, viewer) {
       ])
       table.appendChild(tr)
 
-      removeBtn.addEventListener('click', removeColor.bind(null, layerColors, cpEl.style.backgroundColor, num1.value, num2.value, tr, layers, viewer), {passive: true});
+      removeBtn.addEventListener('click', removeColor.bind(null, layerColors, cpEl.style.backgroundColor, num1.value, num2.value, tr, layers, viewer), {capture: true, passive: true});
 
     })
 
@@ -182,9 +182,9 @@ function createNumericInput(id, uniq, layers, colorLowHi, colors, viewer) {
     value: id.includes('low') ? colorLowHi.low.toString() : colorLowHi.hi.toString()
   })
 
-  x.addEventListener('change', isIntersect.bind(null, uniq, colors.length), {passive: true});
+  x.addEventListener('change', isIntersect.bind(null, uniq, colors.length), {capture: true, passive: true});
 
-  x.addEventListener('input', numericEvent.bind(null, x, colorLowHi, layers, viewer), {passive: true});
+  x.addEventListener('input', numericEvent.bind(null, x, colorLowHi, layers, viewer), {capture: true, passive: true});
   return x
 }
 
@@ -259,7 +259,7 @@ function addEvent(num1, num2, cpEl, uniq, tr, colors, layers, viewer) {
     let removeBtn = e('i', {class: 'fas fa-minus pointer'})
     tr.lastChild.firstChild.remove()
     tr.lastChild.appendChild(removeBtn)
-    removeBtn.addEventListener('click', removeColor.bind(null, colors, cpEl.style.backgroundColor, num1.value, num2.value, tr, layers, viewer), {passive: true});
+    removeBtn.addEventListener('click', removeColor.bind(null, colors, cpEl.style.backgroundColor, num1.value, num2.value, tr, layers, viewer), {capture: true, passive: true});
 
     // add another empty row
     const table = tr.closest('table')
@@ -283,7 +283,7 @@ function extraRow(uniq, colors, layers, viewer) {
     e('td', {}, [addBtn])
   ])
 
-  addBtn.addEventListener('click', addEvent.bind(null, num1, num2, cpEl, uniq, tr, colors, layers, viewer), {passive: true});
+  addBtn.addEventListener('click', addEvent.bind(null, num1, num2, cpEl, uniq, tr, colors, layers, viewer), {capture: true, passive: true});
 
   return tr
 }
