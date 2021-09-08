6ce204f523248e94fe7ade8d8a4b0599bc6a1ccc
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-09-08 15:27:57 -0400

    no need to getTileUrl here

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -36,10 +36,6 @@ class ImageViewer {
       const itemIndex = viewer.world.getIndexOfItem(item)
       let source = viewer.world.getItemAt(itemIndex).source
       if (itemIndex > 0) {
-        // CONFIGURE OUR CUSTOM TILE SOURCES
-        source.getTileUrl = function (level, x, y) {
-          return getIIIFTileUrl(this, level, x, y)
-        }
         // ADD INFO TO OUR ITEMS
         itemsToBeDisplayed[itemIndex].prefLabel = source.prefLabel // set prefLabel
       } else {
@@ -90,34 +86,6 @@ class ImageViewer {
         }
       }
     })
-
-    function getIIIFTileUrl(source, level, x, y) {
-      const scale = Math.pow(0.5, source.maxLevel - level)
-      const levelWidth = Math.ceil(source.width * scale)
-      const levelHeight = Math.ceil(source.height * scale)
-
-      const tileSize = source.getTileWidth(level) // width == height
-      let tileSizeWidth
-      const tileSizeHeight = tileSizeWidth = Math.ceil(tileSize / scale)
-
-      const ROTATION = '0'
-      const quality = 'default.png'
-
-      let region, tileX, tileY, tileW, tileH, size
-
-      if (levelWidth < tileSize && levelHeight < tileSize) {
-        size = levelWidth + ','
-        region = 'full'
-      } else {
-        tileX = x * tileSizeWidth
-        tileY = y * tileSizeHeight
-        tileW = Math.min(tileSizeWidth, source.width - tileX)
-        tileH = Math.min(tileSizeHeight, source.height - tileY)
-        size = Math.ceil(tileW * scale) + ','
-        region = [tileX, tileY, tileW, tileH].join(',')
-      }
-      return [source['@id'], region, size, ROTATION, quality].join('/')
-    }
 
     function useParams(params) {
       let zoom = vpt.getZoom()
