c18eccee6fc9b76c9076faa07e5ec9de6c10e4f1
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-11-11 14:24:50 -0500

    testing

--- testing.js
+++ testing.js
@@ -1,79 +1,74 @@
-function brcaItems(toggle) {
-  let img = 'brca/TCGA-3C-AALI-01Z-00-DX1.F6E9A5DF-D8FB-45CF-B4BD-C6B76294C291'
-  let loc = window.location.origin
-  let base = {
-    layerNum: 0,
-    location: `${loc}/iiif/?iiif=/tcgaseg/tcgaimages/${img}.svs/info.json`,
-    opacity: 0.9
-  }
-  if (toggle) {
-    return [base, {
-      layerNum: 1,
-      location: `${loc}/iiif/?iiif=/tcgaseg/featureimages/${img}-featureimage.tif/info.json'`,
-      opacity: 0.9,
-      colors: [{color: 'rgba(245, 0, 7, 255)', low: 0, hi: 85}, {
-        color: 'rgba(92, 0, 161, 255)',
-        low: 86,
-        hi: 171
-      }, {color: 'rgba(10, 0, 244, 255)', low: 172, hi: 255}]
-    }]
-  } else {
-    return [base, {
-      layerNum: 1,
-      location: `${loc}/iiif/?iiif=/tcgaseg/featureimages/brca/TCGA-3C-AALJ-01Z-00-DX1.777C0957-255A-42F0-9EEB-A3606BCF0C96-featureimage.tif/info.json`,
-      opacity: 0.9,
-      colors: [{color: 'rgba(254, 213, 2, 255)', low: 0, hi: 85}, {
-        color: 'rgba(227, 98, 53, 255)',
-        low: 86,
-        hi: 171
-      }, {color: 'rgba(60, 1, 114, 255)', low: 172, hi: 255}]
-    }]
-  }
-}
+/*
+An array of viewers
+[
+    [],
+    [],
+    [],
+    []
+]
 
-function testItems(toggle) {
-  let img = '//openseadragon.github.io/example-images/duomo/duomo.dzi'
-  let base = {
-    layerNum: 0,
-    location: img,
-    opacity: 0.5
-  }
-  if (toggle) {
-    return [base, {
-      layerNum: 1,
-      location: img,
-      opacity: 0.7,
-      colors: [{color: 'rgba(245, 0, 7, 255)', low: 0, hi: 85}, {
-        color: 'rgba(92, 0, 161, 255)',
-        low: 86,
-        hi: 171
-      }, {color: 'rgba(10, 0, 244, 255)', low: 172, hi: 255}]
-    }]
-  } else {
-    return [base, {
-      layerNum: 1,
-      location: img,
-      opacity: 0.7,
-      colors: [{color: 'rgba(254, 213, 2, 255)', low: 0, hi: 85}, {
-        color: 'rgba(227, 98, 53, 255)',
-        low: 86,
-        hi: 171
-      }, {color: 'rgba(60, 1, 114, 255)', low: 172, hi: 255}]
-    }]
-  }
+A viewer contains
+{
+    "layerNum": 0,
+    "location": "http://129.49.255.69/iiif/?iiif=http://129.49.255.69/HalcyonStorage/TCGA-2J-AAB1-01Z-00-DX1.F3B4818F-9C3B-4C66-8241-0570B2873EC9.svs/info.json",
+    "opacity": 0.3
+},
+{
+    "layerNum": 1,
+    "location": "http://129.49.255.69/halcyon/?iiif=file:///data/HalcyonStorage/hovernet.zip/info.json",
+    "opacity": 0.3,
+    "colors": [
+        { "color": "rgba(255, 140, 0, 255)", "low": 206, "hi": 255 },
+        { "color": "rgba(255, 140, 0, 255)", "low": 206, "hi": 255 },
+        { "color": "rgba(255, 140, 0, 255)", "low": 206, "hi": 255 }
+    ]
 }
+ */
 
-const options = {
+let viewer = [
+  [{
+    "layerNum": 0,
+    "location": "//openseadragon.github.io/example-images/duomo/duomo.dzi",
+    "opacity": 0.3
+  },
+    {
+      "layerNum": 1,
+      "location": "//openseadragon.github.io/example-images/duomo/duomo.dzi",
+      "opacity": 0.3,
+      "colors": [
+        {
+          "value": 1,
+          "label": "Tumor",
+          "color": "rgba(255, 255, 0, 255)",
+          "low": 206,
+          "hi": 255
+        },
+        {
+          "value": 2,
+          "label": "Miscellaneous",
+          "color": "rgba(0, 0, 255, 255)",
+          "low": 160,
+          "hi": 205
+        },
+        {
+          "value": 3,
+          "label": "Lymphocyte",
+          "color": "rgba(255, 0, 0, 255)",
+          "low": 151,
+          "hi": 159
+        },
+        {
+          "value": 4,
+          "label": "https://null.com/background",
+          "color": "rgba(255, 165, 0, 255)",
+          "low": 106,
+          "hi": 150
+        }
+      ]
+    }]
+]
+
+pageSetup('contentDiv', viewer, 1, 1, 1, 640, 480, {
   toolbarOn: true,
   paintbrushColor: '#0ff'
-}
-
-// 2 VIEWERS.  Each element represents each viewer's array of images to be displayed.
-let images = [testItems(true), testItems(false)]
-// let images = [brcaItems(true), brcaItems(false)]
-pageSetup('contentDiv', images, 2, 1, 2, 640, 480, options)
-
-// 1 VIEWER.
-// let viewerSlides = [itemsToBeDisplayed]
-// pageSetup('contentDiv', viewerSlides, 1, 1, 1, 640, 480, options)
-
+})
