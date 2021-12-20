5b7f42a5d4792b108b6384ce5a230dcb714f89a3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2021-12-20 11:23:13 -0500

    cleanup

--- src/layers.js
+++ src/layers.js
@@ -239,66 +239,17 @@ function addRow(table, currentLayer, allLayers, viewer) {
     let dd = e('div', {}, [attenuation, fillPoly, section])
     body.appendChild(dd)
 
-    // probability off/on
-    // let probability = e('i', {
-    //   'id': makeId(5, 'prob'),
-    //   'class': `fas fa-shapes hover-light ${myClass}`,
-    //   'title': 'toggle: class / probability'
-    // })
-    // tr.appendChild(e('td', {}, [probability]))
-    // //childDiv.appendChild(probability)
-    // probability.addEventListener('click', () => {
-    //   let pi = colorsUI.id.replace('filters', '')
-    //   if (renderType === 'byClass') {
-    //     renderType = 'byProbability'
-    //     document.getElementById(`divA${pi}`).style.display = 'none'
-    //     document.getElementById(`divB${pi}`).style.display = 'block'
-    //   } else {
-    //     renderType = 'byClass'
-    //     document.getElementById(`divA${pi}`).style.display = 'block'
-    //     document.getElementById(`divB${pi}`).style.display = 'none'
-    //   }
-    //   outlineFlag = false
-    //   toggleButton(probability, 'fa-shapes', 'fa-dice')
-    //   setFilter(allLayers, viewer)
-    // })
-
-    // heatmap off/on
-    // let heatmap = e('i', {
-    //   'id': makeId(5, 'prob'),
-    //   'class': `far fa-map hover-light ${myClass}`,
-    //   'title': 'blue-red heatmap'
-    // })
-    // tr.appendChild(e('td', {}, [heatmap]))
-    // // childDiv.appendChild(heatmap)
-    // heatmap.addEventListener('click', () => {
-    //   if (renderType === 'byHeatmap') {
-    //     if (probability.classList.contains('fa-shapes')) {
-    //       renderType = 'byClass'
-    //     }
-    //     if (probability.classList.contains('fa-dice')) {
-    //       renderType = 'byProbability'
-    //     }
-    //   } else {
-    //     renderType = 'byHeatmap'
-    //   }
-    //   outlineFlag = false
-    //
-    //   setFilter(allLayers, viewer)
-    // })
-
-
   } else {
     tr.appendChild(e('td'))
   }
 }
 
 function createLayerWidget(div, layers, viewer) {
-  // todo: comment
+  // todo: if switch to divs, comment next 2 lines:
   let table = e('table')
   div.appendChild(table)
   layers.forEach(layer => {
-    // todo: swap
+    // todo: ...and comment the 'table', uncomment the 'div':
     addRow(table, layer, layers, viewer)
     //addRow(div, layer, layers, viewer)
   })
