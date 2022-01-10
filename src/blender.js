a08926ec6c314613d027a1938962d572061f09ef
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-01-10 15:18:45 -0500

    Definitions (1st draft)

--- src/blender.js
+++ src/blender.js
@@ -10,22 +10,22 @@
  */
 const blender = (blenderBtn, viewer) => {
   const blendModes = [
-    'normal',
-    'difference',
-    'multiply',
-    'screen',
-    'overlay',
-    'darken',
-    'lighten',
-    'color-dodge',
-    'color-burn',
-    'hard-light',
-    'soft-light',
-    'exclusion',
-    'hue',
-    'saturation',
-    'color',
-    'luminosity'
+    ['normal', 'normal'],
+    ['difference', 'The Difference blend mode subtracts the pixels of the base and blend layers and the result is the greater brightness value.'],
+    ['multiply', 'The Multiply mode multiplies the colors of the blending layer and the base layers, resulting in a darker color.'],
+    ['screen','With Screen blend mode, the values of the pixels in the layers are inverted, multiplied, and then inverted again. The result is the opposite of Multiply: wherever either layer was darker than white, the composite is brighter.'],
+    ['overlay', 'The Overlay blend mode both multiplies dark areas and screens light areas at the same time, so dark areas become darker and light areas become lighter.'],
+    ['darken', 'The Darken blending mode does not blend pixels; it only compares the layers and keeps the darkest.'],
+    ['lighten', 'The Lighten blending mode takes a look at the layers, and it keeps whichever is lightest.'],
+    ['color-dodge', 'The Color Dodge blend mode divides the bottom layer by the inverted top layer.'],
+    ['color-burn', 'The Color Burn Blending Mode gives you a darker result than Multiply by increasing the contrast between the base and the blend colors resulting in more highly saturated mid-tones and reduced highlights.'],
+    ['hard-light', 'Hard Light combines the Multiply and Screen Blending Modes using the brightness values of the Blend layer to make its calculations. The results with Hard Light tend to be intense.'],
+    ['soft-light', 'With the Soft Light blending mode, every color that is lighter than 50% grey will get even lighter, like it would if you shine a soft spotlight to it. In the same way, every color darker than 50% grey will get even darker.'],
+    ['exclusion', 'Exclusion is very similar to Difference. Blending with white inverts the base color values, while blending with black produces no change. However, Blending with 50% gray produces 50% gray.'],
+    ['hue', 'The Hue Blending Mode preserves the luminosity and saturation of the base pixels while adopting the hue of the blend pixels.'],
+    ['saturation', 'The Saturation Blending Mode preserves the luminosity and hue of the base layer while adopting the saturation of the blend layer.'],
+    ['color', 'The Color blend mode is a combination of Hue and Saturation. Only the color (the hues and their saturation values) from the layer is blended in with the layer or layers below it.'],
+    ['luminosity', 'The Luminosity blend mode preserves the hue and chroma of the bottom layers, while adopting the luma of the top layer.']
   ]
   let uiCreated = false
 
@@ -35,17 +35,20 @@ const blender = (blenderBtn, viewer) => {
     div.appendChild(table)
 
     blendModes.forEach(item => {
+      let name = item[0]
+      let def = item[1]
       const blendBtn = e('button', {
         'type': 'button',
-        'id': item.replace('-', '_'),
-        'value': item,
-        'class': 'btn hover-light',
-        'style': 'width: 120px'
+        'id': name.replace('-', '_'),
+        'value': name,
+        'class': 'btn hover-light css-tooltip',
+        'style': 'width: 120px',
+        'data-tooltip': def
       })
-      blendBtn.innerHTML = item
+      blendBtn.innerHTML = name
 
       const row = e('tr', {}, [
-        e('td', {}, [blendBtn, e('br')])
+        e('td', {'class':'tooltip'}, [blendBtn, e('br')])
       ])
       table.appendChild(row)
 
