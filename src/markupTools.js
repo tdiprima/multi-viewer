bc9b43d43d3a40fdf5071a831e9f75786d60a979
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-01-13 09:44:04 -0500

    readability

--- src/markupTools.js
+++ src/markupTools.js
@@ -10,10 +10,14 @@
 const markupTools = (viewerInfo, viewer) => {
   const overlay = viewer.fabricjsOverlay({scale: 1, static: false})
   const idx = viewerInfo.idx
+
   drawPolygon(viewerInfo, viewer, overlay)
+
   editPolygon(document.getElementById(`btnEdit${idx}`), overlay)
-  gridOverlay(document.getElementById(`btnGrid${idx}`),
-    document.getElementById(`btnGridMarker${idx}`), overlay)
+
+  gridOverlay(document.getElementById(`btnGrid${idx}`), document.getElementById(`btnGridMarker${idx}`), overlay)
+
   ruler(document.getElementById(`btnRuler${idx}`), viewer, overlay)
+
   blender(document.getElementById(`btnBlender${idx}`), viewer)
 }
