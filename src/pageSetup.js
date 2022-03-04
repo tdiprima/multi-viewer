7e5b95ce308441b17febb16a8e06a0ee83a599ca
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-03-04 11:35:57 -0500

    Update pageSetup.js

--- src/pageSetup.js
+++ src/pageSetup.js
@@ -103,16 +103,16 @@ const pageSetup = (divId, images, numViewers, rows, columns, width, height, opts
               htm += `<mark id="mark${idx}">#00f</mark>&nbsp;`
             }
 
-            htm += `<button id="btnDraw${idx}" class="btn hover-light" title="Draw"><i class="fas fa-pencil-alt"></i></button>&nbsp;
-<button id="btnEdit${idx}" class="btn hover-light" title="Edit"><i class="fas fa-draw-polygon"></i></button>&nbsp;
-<button id="btnGrid${idx}" class="btn hover-light" title="Grid"><i class="fas fa-border-all"></i></button>&nbsp;
-<button id="btnGridMarker${idx}" class="btn hover-light" title="Mark grid"><i class="fas fa-paint-brush"></i></button>&nbsp;
-<button id="btnRuler${idx}" class="btn hover-light" title="Measure in microns"><i class="fas fa-ruler"></i></button>&nbsp;
-<button id="btnShare${idx}" class="btn hover-light" title="Share this link"><i class="fas fa-share-alt"></i></button>&nbsp;
-<button id="btnCam${idx}" class="btn hover-light" title="Snapshot"><i class="fas fa-camera"></i></button>&nbsp;
-<button id="btnBlender${idx}" class="btn hover-light" title="Blend-modes"><i class="fas fa-blender"></i></button>&nbsp;
-<button id="btnCrosshairs${idx}" class="btn hover-light" title="Crosshairs"><i class="fas fa-crosshairs"></i></button>&nbsp;
-<button id="btnSave${idx}" class="btn hover-light" title="Save"><i class="fas fa-save"></i></button>&nbsp;
+            htm += `<button id="btnDraw${idx}" class="btn hover-light" title="Draw"><i class="fas fa-pencil-alt"></i></button>
+<button id="btnEdit${idx}" class="btn hover-light" title="Edit"><i class="fas fa-draw-polygon"></i></button>
+<button id="btnGrid${idx}" class="btn hover-light" title="Grid"><i class="fas fa-border-all"></i></button>
+<button id="btnGridMarker${idx}" class="btn hover-light" title="Mark grid"><i class="fas fa-paint-brush"></i></button>
+<button id="btnRuler${idx}" class="btn hover-light" title="Measure in microns"><i class="fas fa-ruler"></i></button>
+<button id="btnShare${idx}" class="btn hover-light" title="Share this link"><i class="fas fa-share-alt"></i></button>
+<button id="btnCam${idx}" class="btn hover-light" title="Snapshot"><i class="fas fa-camera"></i></button>
+<button id="btnBlender${idx}" class="btn hover-light" title="Blend-modes"><i class="fas fa-blender"></i></button>
+<button id="btnCrosshairs${idx}" class="btn hover-light" title="Crosshairs"><i class="fas fa-crosshairs"></i></button>
+<button id="btnSave${idx}" class="btn hover-light" title="Save"><i class="fas fa-save"></i></button>
 <div class="mag" style="display: inline">
   <button class="btn hover-light">
   <i class="fas fa-search"></i>
