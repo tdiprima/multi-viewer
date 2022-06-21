ec1761703da630a799b1de8960bda55e3b5b98d6
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-06-21 16:29:56 -0400

    simplify

--- src/drawPolygon.js
+++ src/drawPolygon.js
@@ -6,29 +6,29 @@
  * @param {object} overlay - Canvas on which to draw the polygon
  */
 const drawPolygon = (viewerInfo, viewer, overlay) => {
-  const idx = viewerInfo.idx;
-  const btnDraw = document.getElementById(`btnDraw${idx}`);
-  const mark = document.getElementById(`mark${idx}`);
-  const canvas = overlay.fabricCanvas();
-  // const canvas = this.__canvas = overlay.fabricCanvas(); // for testing.
-  const paintBrush = canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
+  let idx = viewerInfo.idx;
+  let btnDraw = document.getElementById(`btnDraw${idx}`);
+  let mark = document.getElementById(`mark${idx}`);
+  let canvas = overlay.fabricCanvas();
+  // let canvas = this.__canvas = overlay.fabricCanvas(); // for testing.
+  let paintBrush = canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
   paintBrush.decimate = 12;
   paintBrush.color = mark.innerHTML;
 
   canvas.on('mouse:over', evt => {
     if (evt.target !== null) {
-      fillPolygon(evt, canvas);
+      fillPolygon(evt, canvas, true);
     }
   });
 
   canvas.on('mouse:out', evt => {
     if (evt.target !== null) {
-      unfillPolygon(evt, canvas);
+      fillPolygon(evt, canvas, false);
     }
   });
 
   canvas.on('mouse:up', () => {
-    turnDrawingOff(canvas, viewer);
+    drawingOff(canvas, viewer);
   });
 
   canvas.on('path:created', opts => {
@@ -37,30 +37,27 @@ const drawPolygon = (viewerInfo, viewer, overlay) => {
 
   btnDraw.addEventListener('click', function() {
     toggleButton(this, 'btnOn', 'annotationBtn');
-    // Toggle drawing
     if (canvas.isDrawingMode) {
-      turnDrawingOff(canvas, viewer);
+      // Drawing off
+      drawingOff(canvas, viewer);
     } else {
-      turnDrawingOn(canvas, viewer, paintBrush, mark);
+      // Drawing on
+      canvas.isDrawingMode = true;
+      canvas.on('mouse:down', () => {
+        setGestureSettings(canvas, viewer);
+      });
+      paintBrush.color = mark.innerHTML;
+      paintBrush.width = 10 / viewer.viewport.getZoom(true);
+      setOsdTracking(viewer, false);
     }
   });
 
-  function turnDrawingOff(canvas, viewer) {
+  function drawingOff(canvas, viewer) {
     canvas.isDrawingMode = false;
     canvas.off('mouse:down', () => {
       setGestureSettings(canvas, viewer);
     });
     setOsdTracking(viewer, true);
-  }
-
-  function turnDrawingOn(canvas, viewer, paintBrush, mark) {
-    canvas.isDrawingMode = true;
-    canvas.on('mouse:down', () => {
-      setGestureSettings(canvas, viewer);
-    });
-    paintBrush.color = mark.innerHTML;
-    paintBrush.width = 10 / viewer.viewport.getZoom(true);
-    setOsdTracking(viewer, false);
   }
 
   function pathCreatedHandler(options, button, canvas, paintBrush, viewer) {
@@ -83,14 +80,14 @@ const drawPolygon = (viewerInfo, viewer, overlay) => {
   }
 
   function setupDeleteButton() {
-    const deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+    let deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
 
-    const deleteImg = document.createElement('img');
+    let deleteImg = document.createElement('img');
     deleteImg.src = deleteIcon;
 
     function renderIcon(icon) {
       return function renderIcon(ctx, left, top, styleOverride, fabricObject) {
-        const size = this.cornerSize;
+        let size = this.cornerSize;
         ctx.save();
         ctx.translate(left, top);
         ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
@@ -111,9 +108,9 @@ const drawPolygon = (viewerInfo, viewer, overlay) => {
     });
 
     function deleteObject(mouseEvent, transform) {
-      const target = transform.target;
+      let target = transform.target;
       try {
-        const canvas = target.canvas;
+        let canvas = target.canvas;
         canvas.remove(target);
         canvas.requestRenderAll();
       } catch (e) {
@@ -123,12 +120,12 @@ const drawPolygon = (viewerInfo, viewer, overlay) => {
   }
 
   function convertPathToPolygon(pathObject, canvas, paintBrush) {
-    const _points0 = pathObject.path.map(item => ({
+    let _points0 = pathObject.path.map(item => ({
       x: item[1],
       y: item[2]
     }));
 
-    const poly = new fabric.Polygon(_points0, {
+    let poly = new fabric.Polygon(_points0, {
       left: pathObject.left,
       top: pathObject.top,
       fill: '',
@@ -147,51 +144,43 @@ const drawPolygon = (viewerInfo, viewer, overlay) => {
     canvas.remove(pathObject);
   }
 
-  function fillPolygon(pointerEvent, canvas) {
-    if (weHoveredOverPolygon(pointerEvent)) {
-      const obj = pointerEvent.target;
-      obj.set({
-        fill: obj.stroke,
-        opacity: 0.5
-      });
-      // displayInfo(obj, canvas);
+  function fillPolygon(pointerEvent, canvas, fill) {
+    let obj = pointerEvent.target;
+
+    if (isRealValue(obj) && obj.type === 'polygon') {
+      // polygon hover
+      if (fill) {
+        obj.set({
+          fill: obj.stroke,
+          opacity: 0.5
+        });
+        // displayInfo(obj, canvas);
+      } else {
+        obj.set({
+          fill: ''
+        });
+        // canvas.remove(infoText);
+      }
       canvas.renderAll();
     }
   }
 
-  function unfillPolygon(pointerEvent, canvas) {
-    if (weHoveredOverPolygon(pointerEvent)) {
-      const obj = pointerEvent.target;
-      if (obj !== null) {
-        obj.set({
-          fill: ''
-        });
-        // canvas.remove(infoText)
-        canvas.renderAll();
-      }
-    }
-  }
-
-  function weHoveredOverPolygon(pointerEvent) {
-    return isRealValue(pointerEvent.target) && pointerEvent.target.type === 'polygon';
-  }
-
-  // function displayInfo (obj, canvas) {
+  // function displayInfo(obj, canvas) {
   //   // Display some kind of information. TBA.
   //   // Right now this displays what type of object it is. (Polygon, obviously.)
-  //   const type = obj.type
-  //   const left = obj.left
-  //   const top = obj.top
+  //   let type = obj.type;
+  //   let left = obj.left;
+  //   let top = obj.top;
   //
-  //   const infoText = new fabric.Text(type, {
+  //   let infoText = new fabric.Text(type, {
   //     fontSize: 18,
   //     fontFamily: 'Courier',
   //     backgroundColor: 'rgba(102, 102, 102, 0.7)',
   //     stroke: 'rgba(255, 255, 255, 1)',
   //     fill: 'rgba(255, 255, 255, 1)',
-  //     left: left, // pointer.x,
-  //     top: top // pointer.y
-  //   })
-  //   canvas.add(infoText)
+  //     left, // pointer.x,
+  //     top, // pointer.y
+  //   });
+  //   canvas.add(infoText);
   // }
 };
