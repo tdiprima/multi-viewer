58e556508c13db54fade1442b91ab50b1f1a308a
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-07-19 12:38:51 -0400

    annotorious

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -33,6 +33,20 @@ class ImageViewer {
       console.error(e.message);
     }
 
+    /*
+    let anno = OpenSeadragon.Annotorious(viewer, {
+      locale: 'auto',
+      drawOnSingleClick: true,
+      allowEmpty: true
+    });
+    anno.setAuthInfo({
+      id: 'http://www.example.com/tdiprima',
+      displayName: 'tdiprima'
+    });
+    anno.setDrawingTool('rect');
+    anno.setDrawingEnabled(true);
+    */
+
     let drawer;
 
     function addInfo(item) {
