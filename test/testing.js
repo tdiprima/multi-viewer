a8dc474051c59fa20375088f39592bbfe18af6e3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-07-21 15:46:38 -0400

    testing

--- test/testing.js
+++ test/testing.js
@@ -1,5 +1,5 @@
 let appImages = 'images/';
-// let appImages = '/multi-viewer/images/'
+// let appImages = '/multi-viewer/images/';
 
 let baseUrl;
 let layer;
@@ -7,11 +7,11 @@ let layer;
 layer = '//openseadragon.github.io/example-images/duomo/duomo.dzi';
 baseUrl = '//openseadragon.github.io/example-images/highsmith/highsmith.dzi';
 
-// baseUrl = "http://openseadragon.github.io/example-images/duomo/duomo.dzi"
-// baseUrl = '/images/grand-canyon-landscape-overlooking.jpg'
+// baseUrl = "http://openseadragon.github.io/example-images/duomo/duomo.dzi";
+// baseUrl = "./img/smiley.png"; // "/images/grand-canyon-landscape-overlooking.jpg"
 
-// layer = `${appImages}transparent.png`
-// layer = `${appImages}default.png`
+// layer = "./img/transparent.png";
+// layer = "./img/default.png";
 
 // multi-layer
 function do_slides() {
@@ -21,7 +21,7 @@ function do_slides() {
     paintbrushColor: '#0ff'
   });
 }
-// do_slides()
+// do_slides();
 
 function one_slide() {
   let arr = [[{"layerNum":0,"location":baseUrl,"opacity":0.5,"colorscheme":{"@type":"ColorScheme","name":"Default Color Scheme","colors":[{"name":"Tumor","classid":1,"color":"rgba(0, 255, 0, 255)"},{"name":"Background","classid":4,"color":"rgba(0, 0, 255, 255)"},{"name":"Lymphocyte","classid":3,"color":"rgba(255, 0, 0, 255)"},{"name":"Misc","classid":2,"color":"rgba(255, 255, 0, 255)"}],"colorspectrum":[{"color":"rgba(0, 0, 255, 255)","high":159,"low":101},{"color":"rgba(0, 255, 0, 255)","high":205,"low":160},{"color":"rgba(255, 255, 0, 255)","high":100,"low":0},{"color":"rgba(255, 0, 0, 255)","high":255,"low":206}],"@context":[{"so":"https://schema.org/","hal":"https://www.ebremer.com/halcyon/ns/","name":{"@id":"so:name"},"classid":{"@id":"hal:classid"},"color":{"@id":"hal:color"},"colors":{"@id":"hal:colors"},"low":{"@id":"hal:low"},"high":{"@id":"hal:high"},"ColorByCertainty":{"@id":"hal:ColorByCertainty"},"ColorByClassID":{"@id":"hal:ColorByClassID"},"colorspectrum":{"@id":"hal:colorspectrum"},"ColorScheme":{"@id":"hal:ColorScheme"}}]}},{"layerNum":1,"location":layer,"opacity":0.5,"colorscheme":{"@type":"ColorScheme","name":"Default Color Scheme","colors":[{"name":"Tumor","classid":1,"color":"rgba(0, 255, 0, 255)"},{"name":"Misc","classid":2,"color":"rgba(255, 255, 0, 255)"},{"name":"Lymphocyte","classid":3,"color":"rgba(255, 0, 255, 255)"},{"name":"Background","classid":4,"color":"rgba(0, 255, 255, 255)"}],"colorspectrum":[{"color":"rgba(0, 255, 0, 255)","high":205,"low":160},{"color":"rgba(255, 255, 0, 255)","high":100,"low":0},{"color":"rgba(0, 0, 255, 255)","high":159,"low":101},{"color":"rgba(255, 0, 0, 255)","high":255,"low":206}],"@context":[{"so":"https://schema.org/","hal":"https://www.ebremer.com/halcyon/ns/","name":{"@id":"so:name"},"classid":{"@id":"hal:classid"},"color":{"@id":"hal:color"},"colors":{"@id":"hal:colors"},"low":{"@id":"hal:low"},"high":{"@id":"hal:high"},"ColorByCertainty":{"@id":"hal:ColorByCertainty"},"ColorByClassID":{"@id":"hal:ColorByClassID"},"colorspectrum":{"@id":"hal:colorspectrum"},"ColorScheme":{"@id":"hal:ColorScheme"}}]}}]];
@@ -30,7 +30,7 @@ function one_slide() {
     paintbrushColor: '#0ff'
   });
 }
-one_slide()
+one_slide();
 
 /**
  * Using one slide
