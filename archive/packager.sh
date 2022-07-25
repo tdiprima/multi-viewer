b2fa3f703e0e9ac352439c3a510a7eafdf5462e9
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-07-25 15:16:34 -0400

    revive

---
+++ archive/packager.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm ultraviewer.min.js
+find . -type f -name '*.js' -maxdepth 1 -exec cat {} \; >output
+mv output ../dist/ultraviewer.min.js
+
+# $HOME/Documents/workspace/minify/bin/minifyjs ultraviewer.min.js >temp.js
+# rm ultraviewer.min.js; mv temp.js ultraviewer.min.js
+
+subl ../dist/ultraviewer.min.js vendor/ultraviewer.author.js
+open https://javascript-minifier.com/
---
+++ archive/ultraviewer.author.js
@@ -0,0 +1,3 @@
+/*!
+ *  ULTRAVIEWER 2.4.5 by @tdiprima - https://github.com/tdiprima/multi-viewer
+ */
