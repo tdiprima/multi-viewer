8643775263152a87337a3ac40b4e734676d088be
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-08-28 14:19:56 -0400

    handle auto-logout

--- src/pageSetup.js
+++ src/pageSetup.js
@@ -13,7 +13,17 @@
  */
 const pageSetup = (divId, images, numViewers, rows, columns, width, height, opts) => {
   // console.clear();
+  /*
+  When the 'images' parameter becomes an array with null elements,
+  it usually means that the session timed out or is in the process of timeout.
+   */
   let viewers = []; // eslint-disable-line prefer-const
+  if (!isRealValue(images) || images[0] === null) {
+    // You have been logged out
+    document.write("<script>window.alert('You are logged out...');window.location=`${window.location.origin}/account`;</script>");
+    // logout & redirect
+    // document.write("<script>window.alert('Click OK to continue...');window.location=`${window.location.origin}/auth/realms/Halcyon/protocol/openid-connect/logout?redirect_uri=${window.location.origin}`;</script>");
+  }
 
   document.addEventListener('DOMContentLoaded', setUp);
   window.addEventListener('keydown', hotKeysHandler);
@@ -198,6 +208,5 @@ const pageSetup = (divId, images, numViewers, rows, columns, width, height, opts
         });
       }
     }
-
   }
 };
